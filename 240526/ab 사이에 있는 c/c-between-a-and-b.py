c0565f86fcd2600fcd933ac083d950a5cb4422fa
hanseul37 <hanseul37@daum.net>
2024-05-26 22:57:29 +0900

    Title: ab 사이에 있는 c, Time: 152ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240526/ab 사이에 있는 c/c-between-a-and-b.py
@@ -0,0 +1,11 @@
+arr = input().split()
+a, b, c = int(arr[0]), int(arr[1]), int(arr[2])
+ans = False
+for i in range(a, b+1):
+    if i % c == 0:
+        ans = True
+        break
+if ans == True:
+    print('YES')
+else:
+    print('NO')

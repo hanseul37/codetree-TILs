11e68f0ac4a994b5443a9aae166b0c36c3d87789
hanseul37 <hanseul37@daum.net>
2024-06-03 11:20:24 +0900

    Title: 알파벳 출력하기, Time: 144ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240603/알파벳 출력하기/print-alphabet.py
+++ 240603/알파벳 출력하기/print-alphabet.py
@@ -4,6 +4,6 @@ for i in range(n):
     for j in range(i+1):
         print(chr(ord('A') + cnt), end="")
         cnt+=1
-        if cnt == 23:
+        if cnt == 24:
             cnt = 0
     print()

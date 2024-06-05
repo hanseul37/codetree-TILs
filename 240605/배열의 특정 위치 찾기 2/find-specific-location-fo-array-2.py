a873f22ee9cc58726d4e9a0af731c48c717e4419
hanseul37 <hanseul37@daum.net>
2024-06-05 13:41:05 +0900

    Title: 배열의 특정 위치 찾기 2, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240605/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.py
@@ -0,0 +1,11 @@
+arr = list(map(int, input().split()))
+a = b = 0
+for i in range(10):
+    if i % 2 == 0:
+        a += arr[i]
+    else:
+        b += arr[i]
+if a > b:
+    print(a-b)
+else:
+    print(b-a)

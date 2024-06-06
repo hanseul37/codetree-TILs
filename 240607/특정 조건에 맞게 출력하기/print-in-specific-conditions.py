e589d92fbee40a09888934ed4e2aa3b7be3babde
hanseul37 <hanseul37@daum.net>
2024-06-07 07:49:34 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240607/특정 조건에 맞게 출력하기/print-in-specific-conditions.py
@@ -0,0 +1,10 @@
+arr = list(map(int, input().split()))
+for i in arr:
+    if i == 0:
+        break
+    else:
+        if i % 2 == 0:
+            i //= 2
+        else:
+            i += 3
+        print(i, end=" ")

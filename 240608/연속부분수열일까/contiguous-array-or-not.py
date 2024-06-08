55c3f2f827bf7a0f0327664da0bd6058c7b939bc
hanseul37 <hanseul37@daum.net>
2024-06-08 23:30:59 +0900

    Title: 연속부분수열일까, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240608/연속부분수열일까/contiguous-array-or-not.py
+++ 240608/연속부분수열일까/contiguous-array-or-not.py
@@ -5,12 +5,13 @@ bb = list(map(int, input().split()))
 ins = 0
 for i in range(len(bb)):
     if bb[i] not in aa:
-        ins = 2
-
-gap = aa.index(bb[0])
-for i in range(1, len(bb)):
-    if aa[i + gap] != bb[i]:
         ins = 1
+
+if ins == 0:
+    gap = aa.index(bb[0])
+    for i in range(1, len(bb)):
+        if aa[i + gap] != bb[i]:
+            ins = 1
 
 if ins != 0:
     print('No')

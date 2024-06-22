6a8b39e86338c29ef553ba02a4ae9a444dd077b7
hanseul37 <hanseul37@daum.net>
2024-06-22 23:00:45 +0900

    Title: 더 긴 문자열, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240622/더 긴 문자열/longer-string.py
@@ -0,0 +1,7 @@
+words = input().split()
+if len(words[0]) > len(words[1]):
+    print(words[0], len(words[0]))
+elif len(words[0]) == len(words[1]):
+    print('same')
+else:
+    print(words[1], len(words[1]))

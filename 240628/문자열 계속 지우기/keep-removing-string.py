e056c0c4eda028ef3e9eda2d9cf4d7a28954f871
hanseul37 <hanseul37@daum.net>
2024-06-28 13:31:15 +0900

    Title: 문자열 계속 지우기, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240628/문자열 계속 지우기/keep-removing-string.py
+++ 240628/문자열 계속 지우기/keep-removing-string.py
@@ -3,7 +3,7 @@ B = list(input())
 flag = 0
 for _ in range(len(A)):
     for i in range(len(A)):
-        if A[i] == B[0] and len(A) - i > len(B):
+        if A[i] == B[0] and len(A) - i >= len(B):
             flag = 1
             for j in range(1, len(B)):
                 if A[i+j] != B[j]:

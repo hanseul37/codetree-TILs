8d0ef3b2f90dc236c6b5ccea950eb8b5a4241d10
hanseul37 <hanseul37@daum.net>
2024-06-28 13:25:11 +0900

    Title: 문자열 계속 지우기, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240628/문자열 계속 지우기/keep-removing-string.py
@@ -0,0 +1,15 @@
+A = list(input())
+B = list(input())
+flag = 0
+for _ in range(len(A)):
+    for i in range(len(A)):
+        if A[i] == B[0] and len(A) - i > len(B):
+            flag = 1
+            for j in range(1, len(B)):
+                if A[i+j] != B[j]:
+                    flag = 0
+            if flag == 1:
+                for _ in range(len(B)):
+                    A.pop(i)
+                break
+print(''.join(A))

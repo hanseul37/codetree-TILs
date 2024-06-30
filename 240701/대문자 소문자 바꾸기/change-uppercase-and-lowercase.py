aaad9ab3050518d1e21595fd9926001b73f79354
hanseul37 <hanseul37@daum.net>
2024-07-01 07:45:01 +0900

    Title: 대문자 소문자 바꾸기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240701/대문자 소문자 바꾸기/change-uppercase-and-lowercase.py
+++ 240701/대문자 소문자 바꾸기/change-uppercase-and-lowercase.py
@@ -1,6 +1,6 @@
 word = input()
 for elem in word:
-    if ord(elem) > ord('A') and ord(elem) < ord('Z'):
+    if ord(elem) >= ord('A') and ord(elem) =< ord('Z'):
         print(elem.lower(), end='')
-    elif ord(elem) > ord('a') and ord(elem) < ord('z'):
+    elif ord(elem) >= ord('a') and ord(elem) =< ord('z'):
         print(elem.upper(), end='')

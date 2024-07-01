cd39ec913234da36e463bc5a295257cdf4fac6e0
hanseul37 <hanseul37@daum.net>
2024-07-02 00:03:00 +0900

    Title: 최소공배수 구하기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240702/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240702/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,5 +1,7 @@
+def mini(n:int, m:int):
+    for i in range(max(n, m), n * m, max(n, m)):
+        if i % min(n, m) == 0:
+            print(i)
+            break
 n, m = list(map(int, input().split()))
-for i in range(max(n, m), n * m, max(n, m)):
-    if i % min(n, m) == 0:
-        print(i)
-        break
+mini(n, m)

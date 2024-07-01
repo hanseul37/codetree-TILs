58a888a6d1039cc1bfdbe625ec50e8adfc4fd847
hanseul37 <hanseul37@daum.net>
2024-07-02 00:03:59 +0900

    Title: 최소공배수 구하기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240702/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240702/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,5 +1,5 @@
 def mini(n:int, m:int):
-    for i in range(max(n, m), n * m, max(n, m)):
+    for i in range(max(n, m), n * m + 1, max(n, m)):
         if i % min(n, m) == 0:
             print(i)
             break

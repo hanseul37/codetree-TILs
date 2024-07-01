837b8588314c55117f96572258413bd72e19ebc9
hanseul37 <hanseul37@daum.net>
2024-07-02 08:45:55 +0900

    Title: 함수를 이용한 소수 판별, Time: 109ms, Memory: 26MB, Status: Passed - CodeTree

--- 240702/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240702/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -2,6 +2,8 @@ def prime(n: int):
     for i in range(2, n):
         if n % i == 0:
             return False
+    if n == 1:
+        return False
     return True
 
 a, b = list(map(int, input().split()))

c08818eaeb131df96acc9c09991a71a51b2f09b5
hanseul37 <hanseul37@daum.net>
2024-07-02 14:48:22 +0900

    Title: 2021년 날짜의 유무, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240702/2021년 날짜의 유무/with-or-without-2021.py
+++ 240702/2021년 날짜의 유무/with-or-without-2021.py
@@ -9,7 +9,7 @@ def check(M:int, D:int):
             return True
         else:
             return False
-    elif M == 2 or M == 4 or M == 6 or M == 9 or M == 11:
+    elif M == 4 or M == 6 or M == 9 or M == 11:
         if D >= 1 and D <= 30:
             return True
         else:

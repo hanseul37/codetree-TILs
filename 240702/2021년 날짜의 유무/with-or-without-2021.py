23d0c10a47ad596dc384974fb0e2e9041ea6f9ec
hanseul37 <hanseul37@daum.net>
2024-07-02 14:38:25 +0900

    Title: 2021년 날짜의 유무, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240702/2021년 날짜의 유무/with-or-without-2021.py
@@ -0,0 +1,22 @@
+def check(M:int, D:int):
+    if M == 1 or M == 3 or M == 5 or M == 7 or M == 8 or M == 10 or M == 12:
+        if D >= 1 and D <= 31:
+            return True
+        else:
+            return False
+    elif M == 2:
+        if D >= 1 and D <= 28:
+            return True
+        else:
+            return False
+    else:
+        if D >= 1 and D <= 30:
+            return True
+        else:
+            return False
+
+M, D = list(map(int, input().split()))
+if check(M, D):
+    print('Yes')
+else:
+    print('No')

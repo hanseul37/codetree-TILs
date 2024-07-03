f5c7a2c5a7a297a9a7f83d5076f98923bcbdfe08
hanseul37 <hanseul37@daum.net>
2024-07-04 08:46:29 +0900

    Title: 2개씩 그룹짓기, Time: 140ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240704/2개씩 그룹짓기/group-of-pairs.py
+++ 240704/2개씩 그룹짓기/group-of-pairs.py
@@ -2,7 +2,7 @@ n = int(input())
 arr = list(map(int, input().split()))
 arr.sort()
 max = 0
-for i in range(len(arr) // 2):
+for i in range(len(arr) // 2 + 1):
     if arr[i] + arr[-i] > max:
         max = arr[i] + arr[-(i + 1)]
 print(max)

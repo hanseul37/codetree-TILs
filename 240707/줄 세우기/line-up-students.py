954334ce68de05c524e4cf9fd44868bcfe0b0e18
hanseul37 <hanseul37@daum.net>
2024-07-07 21:18:18 +0900

    Title: 줄 세우기, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240707/줄 세우기/line-up-students.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self, height, weight, number):
+        self.height = height
+        self.weight = weight
+        self.number = number
+
+students = []
+n = int(input())
+for i in range(n):
+    height, weight = input().split()
+    students.append(Student(int(height), int(weight), i + 1))
+
+students.sort(key=lambda x:(-x.height, -x.weight, -x.number))
+
+for student in students:
+    print(student.height, student.weight, student.number)

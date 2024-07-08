2863ed8988637836e162414edfaf106b8da4fd7d
hanseul37 <hanseul37@daum.net>
2024-07-08 18:30:22 +0900

    Title: 그 요일은, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240708/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,13 @@
+m1, d1, m2, d2 = list(map(int, input().split()))
+m = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+A = input()
+
+sum = 0
+for i in range(m1, m2):
+    sum += m[i - 1]
+
+sum -= d1
+sum += d2
+
+
+print(sum // 7 + 1)

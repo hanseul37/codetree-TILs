cebae6d9aacf522f50e0a330b15a83992b98efc8
hanseul37 <hanseul37@daum.net>
2024-07-08 17:47:44 +0900

    Title: 요일 맞추기, Time: 129ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240708/요일 맞추기/guess-day-of-week.py
+++ 240708/요일 맞추기/guess-day-of-week.py
@@ -1,11 +1,17 @@
 m1, d1, m2, d2 = list(map(int, input().split()))
 m = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 sum = 0
-for i in range(m1, m2):
-    sum += m[i - 1]
 
-sum -= d1
-sum += d2
+if m1 < m2:
+    for i in range(m1, m2):
+        sum += m[i - 1]
+    sum -= d1
+    sum += d2
+else:
+    for i in range(m2, m1):
+        sum += m[i - 1]
+    sum -= d2
+    sum += d1
 
 if sum % 7 == 0:
     print('Mon')

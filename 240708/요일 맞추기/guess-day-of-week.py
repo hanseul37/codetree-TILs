dc6232d5b6b97dcc77cdeef20e8aece132906c7c
hanseul37 <hanseul37@daum.net>
2024-07-08 17:44:38 +0900

    Title: 요일 맞추기, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240708/요일 맞추기/guess-day-of-week.py
@@ -0,0 +1,23 @@
+m1, d1, m2, d2 = list(map(int, input().split()))
+m = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+sum = 0
+for i in range(m1, m2):
+    sum += m[i - 1]
+
+sum -= d1
+sum += d2
+
+if sum % 7 == 0:
+    print('Mon')
+elif sum % 7 == 1:
+    print('Tue')
+elif sum % 7 == 2:
+    print('Wed')
+elif sum % 7 == 3:
+    print('Thu')
+elif sum % 7 == 4:
+    print('Fri')
+elif sum % 7 == 5:
+    print('Sat')
+else:
+    print('Sun')

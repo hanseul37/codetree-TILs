5f422cd643033bb6a12021ee87dd1790da3e4304
hanseul37 <hanseul37@daum.net>
2024-07-09 09:41:45 +0900

    Title: 최대로 겹치는 지점, Time: 114ms, Memory: 25MB, Status: Passed - CodeTree

--- 240709/최대로 겹치는 지점/maximum-overlapped-points.py
+++ 240709/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -1,11 +1,11 @@
 n = int(input())
-arr = [0 for i in range(100)]
+arr = [0 for i in range(101)]
 for _ in range(n):
     a, b = list(map(int, input().split()))
     for j in range(a, b + 1):
         arr[j] += 1
 
-for i in range(100):
+for i in range(101):
     if arr[i] == max(arr):
         print(arr[i])
         break

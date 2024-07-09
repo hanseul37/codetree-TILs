6bbd812ce6ea80319f1172696d7ccab49b940260
hanseul37 <hanseul37@daum.net>
2024-07-09 10:00:18 +0900

    Title: 왔다 갔던 구역 2, Time: 108ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 240709/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,25 @@
+n = int(input())
+point = 0
+arr = [0 for i in range(2000)]
+for _ in range(n):
+    x, dir = input().split()
+    if dir == 'L':
+        for i in range(point, point - int(x) - 1, -1):
+            arr[i + 1000] += 1
+        point -= int(x)
+    else:
+        for i in range(point, point + int(x) + 1, 1):
+            arr[i + 1000] += 1
+        point += int(x)
+
+cnt = 0
+flag = 0
+for i in range(2000):
+    if arr[i] >= 2:     
+        flag = 1
+        cnt += 1
+    else:
+        if flag == 1:
+            cnt -= 1
+            flag = 0
+print(cnt)

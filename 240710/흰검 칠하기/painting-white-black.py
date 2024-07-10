45eed3c20ab63fb4655f39dcc29f6bfaf77d2bfd
hanseul37 <hanseul37@daum.net>
2024-07-10 09:33:53 +0900

    Title: 흰검 칠하기, Time: 120ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240710/흰검 칠하기/painting-white-black.py
@@ -0,0 +1,46 @@
+n = int(input())
+color = ['' for i in range(200000)]
+point = 0
+for _ in range(n):
+    amount, direction = input().split()
+    amount = int(amount)
+    if direction == 'R':
+        for i in range(amount):
+            if color[point + 100000] == 'W':
+                color[point + 100000] = 'GB'
+            elif color[point + 100000] == 'GW':
+                color[point + 100000] = 'GGB'
+            elif color[point + 100000] == 'GGW':
+                color[point + 100000] = 'G'
+            elif color[point + 100000] == 'G':
+                continue
+            else:
+                color[point + 100000] = 'B' 
+            if i != amount - 1:
+                point += 1 
+    else:
+        for i in range(amount):
+            if color[point + 100000] == 'B':
+                color[point + 100000] = 'GW'
+            elif color[point + 100000] == 'GB':
+                color[point + 100000] = 'GGW'
+            elif color[point + 100000] == 'GGB':
+                color[point + 100000] = 'G'      
+            elif color[point + 100000] == 'G':
+                continue
+            else:
+                color[point + 100000] = 'W' 
+            if i != amount - 1:
+                point -= 1 
+
+w, b, g = 0, 0, 0
+for i in range(200000):
+    if color[i] == '':
+        continue
+    elif color[i][-1] == 'W':
+        w += 1
+    elif color[i][-1] == 'B':
+        b += 1
+    else:
+        g += 1
+print(w, b, g)

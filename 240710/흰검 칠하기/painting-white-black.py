b0222d7eb570912d4ca716277d0352e9efaa915d
hanseul37 <hanseul37@daum.net>
2024-07-10 10:37:04 +0900

    Title: 흰검 칠하기, Time: 107ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240710/흰검 칠하기/painting-white-black.py
+++ 240710/흰검 칠하기/painting-white-black.py
@@ -7,43 +7,31 @@ for _ in range(n):
     amount = int(amount)
     if direction == 'R':
         for i in range(amount):
-            if color[point + 100000] == 'W':
-                color[point + 100000] = 'GB'
-            elif color[point + 100000] == 'GW':
-                color[point + 100000] = 'GGB'
-            elif color[point + 100000] == 'GGW':
-                color[point + 100000] = 'G'
-            elif color[point + 100000] == 'G':
+            if color[point + 100000].count('B') == 2 and color[point + 100000].count('W') == 2:
                 continue
             else:
-                if color[point + 100000] == '': 
-                    color[point + 100000] = 'B' 
+                color[point + 100000] += 'B' 
             if i != amount - 1:
                 point += 1 
     else:
         for i in range(amount):
-            if color[point + 100000] == 'B':
-                color[point + 100000] = 'GW'
-            elif color[point + 100000] == 'GB':
-                color[point + 100000] = 'GGW'
-            elif color[point + 100000] == 'GGB':
-                color[point + 100000] = 'G'      
-            elif color[point + 100000] == 'G':
+            if color[point + 100000].count('B') == 2 and color[point + 100000].count('W') == 2:
                 continue
             else:
-                if color[point + 100000] == '':
-                    color[point + 100000] = 'W' 
+                color[point + 100000] += 'W' 
             if i != amount - 1:
                 point -= 1 
+    for i in range(99998, 100002):
+        print(color[i], end=' ')
 
 w, b, g = 0, 0, 0
 for i in range(200000):
     if color[i] == '':
         continue
+    elif color[i].count('B') == 2 and color[i].count('W') == 2:
+        g += 1
     elif color[i][-1] == 'W':
         w += 1
     elif color[i][-1] == 'B':
         b += 1
-    else:
-        g += 1
 print(w, b, g)

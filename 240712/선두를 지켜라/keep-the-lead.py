17bb06a85cf04a63017e25b1291879acd03278f8
hanseul37 <hanseul37@daum.net>
2024-07-12 08:50:15 +0900

    Title: 선두를 지켜라, Time: 143ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240712/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,30 @@
+n, m = list(map(int, input().split()))
+a, b = [], []
+a_point, b_point = 0, 0
+for _ in range(n):
+    v, t = list(map(int, input().split()))
+    for i in range(t):
+        a_point += v
+        a.append(a_point)
+    
+for _ in range(m):
+    v, t = list(map(int, input().split()))
+    for i in range(t):
+        b_point += v
+        b.append(b_point)
+
+lead = ''
+cnt = 0
+for i in range(len(a)):
+    if a[i] > b[i]:
+        now_lead = 'A'
+    elif a[i] < b[i]:
+        now_lead = 'B'
+    else: 
+        now_lead = lead
+    
+    if lead != now_lead:
+        lead = now_lead
+        cnt += 1
+
+print(cnt - 1)

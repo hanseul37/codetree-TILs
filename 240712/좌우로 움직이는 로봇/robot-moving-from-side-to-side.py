47a65f620d35f777151f351ac1beb3101d03a96d
hanseul37 <hanseul37@daum.net>
2024-07-12 09:43:44 +0900

    Title: 좌우로 움직이는 로봇, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240712/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -0,0 +1,42 @@
+n, m = list(map(int, input().split()))
+a, b = [], []
+a_point, b_point = 0, 0
+for _ in range(n):
+    t, d = input().split()
+    if d == 'L':
+        for i in range(int(t)):
+            a_point -= 1
+            a.append(a_point)
+    else:
+        for i in range(int(t)):
+            a_point += 1
+            a.append(a_point)
+
+for _ in range(m):
+    t, d = input().split()
+    if d == 'L':
+        for i in range(int(t)):
+            b_point -= 1
+            b.append(b_point)
+    else:
+        for i in range(int(t)):
+            b_point += 1
+            b.append(b_point)
+
+cnt = 0
+if len(a) > len(b):
+    max_len = len(a)
+else:
+    max_len = len(b)
+for i in range(max_len):
+    if len(a) > len(b) and i > len(b) - 1:
+        if a[i] == b[-1] and a[i - 1] != b[-2]:
+            cnt += 1
+    elif len(a) < len(b) and i > len(a) - 1:
+        if a[-1] == b[i] and a[-2] != b[i - 1]:
+            cnt += 1
+    else:
+        if a[i] == b[i] and a[i - 1] != b[i - 1]:
+            cnt += 1
+
+print(cnt)

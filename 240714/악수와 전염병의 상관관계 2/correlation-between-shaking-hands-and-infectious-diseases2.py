8a925fe9e107b0813fb0deca16f086eafc7e2b30
hanseul37 <hanseul37@daum.net>
2024-07-14 04:16:34 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 173ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240714/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,33 @@
+class Shake:
+    def __init__(self, t, x, y):
+        self.t = t
+        self.x =x
+        self.y = y
+    
+N, K, P, T = list(map(int, input().split()))
+timeline = []
+infection = [0 for _ in range(N)]
+infection[P - 1] = 1
+count = [0 for _ in range(N)]
+
+for _ in range(T):
+    t, x, y = list(map(int, input().split()))
+    timeline.append(Shake(t, x, y))
+
+timeline.sort(key=lambda x:x.t)
+
+for time in timeline:
+    if infection[time.x - 1] == 1 and infection[time.y - 1] == 1:
+        count[time.x - 1] += 1
+        count[time.y - 1] += 1
+    elif infection[time.x - 1] == 1 and infection[time.y - 1] == 0:
+        if count[time.x - 1] < K:
+            infection[time.y - 1] = 1
+            count[time.x - 1] += 1
+    elif infection[time.x - 1] == 0 and infection[time.y - 1] == 1:
+        if count[time.y - 1] < K:
+            infection[time.x - 1] = 1
+            count[time.y - 1] += 1
+
+for infect in infection:
+    print(infect, end='')

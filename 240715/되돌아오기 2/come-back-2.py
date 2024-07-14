60dd1e373bdd66f254e0a374cb4d6eb76fb09a8f
hanseul37 <hanseul37@daum.net>
2024-07-15 08:37:16 +0900

    Title: 되돌아오기 2, Time: 119ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240715/되돌아오기 2/come-back-2.py
@@ -0,0 +1,20 @@
+x, y = 0, 0
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+order = input()
+direction = 0
+find = False
+for i in range(len(order)):
+    if order[i] == 'L':
+        direction = (direction - 1) % 4
+    elif order[i] == 'R':
+        direction = (direction + 1) % 4
+    else:
+        x += dx[direction]
+        y += dy[direction]
+    
+    if x == 0 and y == 0:
+        find = True
+        print(i + 1)
+
+if not find:
+    print(-1)

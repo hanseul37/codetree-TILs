a9b9d360deab785950857a34e95094a59bd68de4
hanseul37 <hanseul37@daum.net>
2024-07-16 08:46:36 +0900

    Title: 체크판위에서 2, Time: 150ms, Memory: 25MB, Status: Passed - CodeTree

--- 240716/체크판위에서 2/on-the-checkboard-2.py
+++ 240716/체크판위에서 2/on-the-checkboard-2.py
@@ -9,6 +9,6 @@ for i in range(1, r - 2):
     for j in range(1, c - 2):
         for k in range(i + 1, r - 1):
             for l in range(j + 1, c - 1):
-                if arr[i][j] != arr[0][0] and arr[k][l] != arr[r - 1][c - 1]:
+                if arr[i][j] != arr[0][0] and arr[k][l] != arr[r - 1][c - 1] and arr[0][0] != arr[r - 1][c - 1]:
                     cnt += 1 
 print(cnt)

278b7d5b607383aac514856410c1251996499228
hanseul37 <hanseul37@daum.net>
2024-07-16 08:45:32 +0900

    Title: 체크판위에서 2, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240716/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,14 @@
+r, c = list(map(int, input().split()))
+arr = []
+for _ in range(r):
+    row = input().split()
+    arr.append(row)
+
+cnt = 0
+for i in range(1, r - 2):
+    for j in range(1, c - 2):
+        for k in range(i + 1, r - 1):
+            for l in range(j + 1, c - 1):
+                if arr[i][j] != arr[0][0] and arr[k][l] != arr[r - 1][c - 1]:
+                    cnt += 1 
+print(cnt)

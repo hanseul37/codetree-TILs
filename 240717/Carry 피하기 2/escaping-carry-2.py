3eb79c875f65e58828c3e970365ba00be61cf5f1
hanseul37 <hanseul37@daum.net>
2024-07-17 16:15:43 +0900

    Title: Carry 피하기 2, Time: 174ms, Memory: 25MB, Status: Passed - CodeTree

--- 240717/Carry 피하기 2/escaping-carry-2.py
+++ 240717/Carry 피하기 2/escaping-carry-2.py
@@ -5,17 +5,21 @@ for _ in range(n):
     arr.append(num)
 
 
-max = -1
+max_value = -1
 for i in range(n - 2):
     for j in range(i + 1, n - 1):
         for k in range(j + 1, n):
             carry = 0
-            for l in range(min(len(arr[i]), len(arr[j]), len(arr[k]))):
+            max_len = max(len(arr[i]), len(arr[j]), len(arr[k]))
+            for l in range(max_len):
+                arr[i] = arr[i].zfill(max_len)
+                arr[j] = arr[j].zfill(max_len)
+                arr[k] = arr[k].zfill(max_len)
                 if int(arr[i][-(l + 1)]) + int(arr[j][-(l + 1)]) + int(arr[k][-(l + 1)]) >= 10:
                     carry = 1
             if carry == 0:
                 sum = int(arr[i]) + int(arr[j]) + int(arr[k])
-                if max < sum:
-                    max = sum
+                if max_value < sum:
+                    max_value = sum
 
-print(max)
+print(max_value)

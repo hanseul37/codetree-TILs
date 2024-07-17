aad5cda588c9762919cb84f51a480a927382195a
hanseul37 <hanseul37@daum.net>
2024-07-18 08:49:09 +0900

    Title: 오목, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240718/오목/O-mok.py
@@ -0,0 +1,64 @@
+board = []
+for _ in range(19):
+    room = input().split()
+    board.append(room)
+
+def in_range1(x, y):
+    for i in range(1, 5):
+        if not 0 <= x < 19 and 0 <= y + i < 19:
+            return False
+    return True
+
+def in_range2(x, y):
+    for i in range(1, 5):
+        if not 0 <= x + i < 19 and 0 <= y < 19:
+            return False
+    return True
+
+def in_range3(x, y):
+    for i in range(1, 5):
+        if not 0 <= x + i < 19 and 0 <= y + i < 19:
+            return False
+    return True
+
+def in_range4(x, y):
+    for i in range(1, 5):
+        if not 0 <= x - i < 19 and 0 <= y + i < 19:
+            return False
+    return True
+
+winner = 0
+x, y = -1, -1
+for i in range(19):
+    for j in range(19):
+        if board[i][j] == '1':
+            if in_range1(j, i) and board[i + 1][j] == '1' and board[i + 2][j] == '1' and board[i + 3][j] == '1' and board[i + 4][j] == '1':
+                winner = 1
+                y, x = i + 3, j + 1
+            elif in_range2(j, i) and board[i][j + 1] == '1' and board[i][j + 2] == '1' and board[i][j + 3] == '1' and board[i][j + 4] == '1':
+                winner = 1
+                y, x = i + 1, j + 3
+            elif in_range3(j, i) and board[i + 1][j + 1] == '1' and board[i + 2][j + 2] == '1' and board[i + 3][j + 3] == '1' and board[i + 4][j + 4] == '1':
+                winner = 1
+                y, x = i + 3, j + 3
+            elif in_range4(j, i) and board[i + 1][j - 1] == '1' and board[i + 2][j - 2] == '1' and board[i + 3][j - 3] == '1' and board[i + 4][j - 4] == '1':
+                winner = 1
+                y, x = i + 3, j - 1
+
+        elif board[i][j] == '2':
+            if in_range1(j, i) and board[i + 1][j] == '2' and board[i + 2][j] == '2' and board[i + 3][j] == '2' and board[i + 4][j] == '2':
+                winner = 2
+                y, x = i + 3, j + 1
+            elif in_range2(j, i) and board[i][j + 1] == '2' and board[i][j + 2] == '2' and board[i][j + 3] == '2' and board[i][j + 4] == '2':
+                winner = 2
+                y, x = i + 1, j + 3
+            elif in_range3(j, i) and board[i + 1][j + 1] == '2' and board[i + 2][j + 2] == '2' and board[i + 3][j + 3] == '2' and board[i + 4][j + 4] == '2':
+                winner = 2
+                y, x = i + 3, j + 3
+            elif in_range4(j, i) and board[i + 1][j - 1] == '2' and board[i + 2][j - 2] == '2' and board[i + 3][j - 3] == '2' and board[i + 4][j - 4] == '2':
+                winner = 2
+                y, x = i + 3, j - 1
+
+print(winner)
+if winner != 0:
+    print(y, x)

8b95cf53fbdf9b0675a5e7a952a1f859ec65263b
hanseul37 <hanseul37@daum.net>
2024-07-19 14:31:57 +0900

    Title: 아름다운 수열 2, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240719/아름다운 수열 2/beautiful-sequence-2.py
+++ 240719/아름다운 수열 2/beautiful-sequence-2.py
@@ -3,15 +3,13 @@ a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 cnt = 0
 for i in range(len(a) - len(b) + 1):
+    a_arr = sorted(a[i:i + len(b)])
+    b_arr = sorted(b)
     flag = 0
-    for j in range(i, i + len(b)):
-        if not a[j] in b:
+    for j in range(len(b)):
+        if a_arr[j] != b_arr[j]:
             flag = 1
             break
-    for j in range(len(b)):
-        if not b[j] in a[i:i + len(b)]:
-            flag = 1
-            break 
-    if not flag:
+    if flag == 0:
         cnt += 1
 print(cnt)

c715edf11da68c58102be1273521eda7176c135e
hanseul37 <hanseul37@daum.net>
2024-07-29 08:00:17 +0900

    Title: 스승의 은혜 2, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240729/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,22 @@
+n, b = list(map(int, input().split()))
+students = []
+for _ in range(n):
+    price = int(input())
+    students.append(price)
+
+max_cnt = 0
+for i in range(n):
+    sum_price = 0
+    cnt = 0
+    for j in range(n):
+        if i == j:
+            sum_price += students[j] // 2
+        else:
+            sum_price += students[j]
+        cnt += 1
+        
+        if sum_price > b:
+            cnt -= 1
+            break
+    max_cnt = max(max_cnt, cnt)
+print(max_cnt)

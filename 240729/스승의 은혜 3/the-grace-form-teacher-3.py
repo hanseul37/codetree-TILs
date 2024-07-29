26078edcb5b9fb7ed3c50428d36c85f3d7a5174d
hanseul37 <hanseul37@daum.net>
2024-07-29 19:09:31 +0900

    Title: 스승의 은혜 3, Time: 185ms, Memory: 73MB, Status: Passed - CodeTree

--- 240729/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240729/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,27 +1,23 @@
 n, b = list(map(int, input().split()))
 students = []
 for _ in range(n):
-    p, s = map(int, input().split())
-    students.append((p, s))
+    student = list(map(int, input().split()))
+    students.append(student)
 
 max_cnt = 0
-
 for i in range(n):
-    discounted_cost = (students[i][0] // 2) + students[i][1]
-    other_costs = [(students[j][0] + students[j][1]) for j in range(n) if j != i]
+    discount = students[i][0] // 2 + students[i][1]
+    other = [(students[j][0] + students[j][1]) for j in range(n) if i != j]
 
-    total_costs = sorted([discounted_cost] + other_costs)
-    
+    total_costs = sorted([discount] + other)
+
     sum_price = 0
     cnt = 0
-    
     for cost in total_costs:
         if sum_price + cost <= b:
             sum_price += cost
             cnt += 1
         else:
             break
-
     max_cnt = max(max_cnt, cnt)
-
 print(max_cnt)

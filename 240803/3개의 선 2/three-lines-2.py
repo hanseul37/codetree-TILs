ee64ffe85a2088cb45aba6c6b7b9ce51d3477fe3
hanseul37 <hanseul37@daum.net>
2024-08-03 16:44:45 +0900

    Title: 3개의 선 2, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240803/3개의 선 2/three-lines-2.py
+++ 240803/3개의 선 2/three-lines-2.py
@@ -13,21 +13,25 @@ for _ in range(n):
 if len(x_arr) <= 3 or len(y_arr) <= 3:
     print(1)
 else:
-    flag = 0
+    find = 0
     for i in range(len(y_arr) - 1):
         for j in range(i + 1, len(y_arr)):
             for k in range(len(x_arr)):
                 a, b, c = y_arr[i], y_arr[j], x_arr[k]
-                for y in range(11):
-                    for x in range(11):
-                        if y == a or y == b or x == c:
-                            flag = 1
+                flag = 1
+                for point in points:
+                    if not (point[1] == a or point[1] == b or point[0] == c):
+                        flag = 0
+                if flag == 1:
+                   find = 1
     for i in range(len(x_arr) - 1):
         for j in range(i + 1, len(x_arr)):
             for k in range(len(y_arr)):
                 a, b, c = x_arr[i], x_arr[j], y_arr[k]
-                for y in range(11):
-                    for x in range(11):
-                        if x == a or x == b or y == c:
-                            flag = 1
-    print(flag)
+                flag = 1
+                for point in points:
+                    if not (point[0] == a or point[0] == b or point[1] == c):
+                        flag = 0
+                if flag == 1:
+                    find = 1
+    print(find)

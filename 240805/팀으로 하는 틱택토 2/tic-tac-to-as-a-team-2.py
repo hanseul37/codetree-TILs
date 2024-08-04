125698b3d4c9990d4691c0ebfb01e66f0f4a2db9
hanseul37 <hanseul37@daum.net>
2024-08-05 08:24:46 +0900

    Title: 팀으로 하는 틱택토 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240805/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 240805/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -3,39 +3,46 @@ for _ in range(3):
     line = list(map(int, input()))
     lines.append(line)
 
-teams = 0
+teams = []
+def check(cnt):
+    return cnt not in teams
+
 for i in range(3):
-    cnt = 0
     point = lines[i][0]
+    cnt = []
+    cnt.append(point)
     for j in range(3):
         if lines[i][j] != point:
-            cnt += 1
-    if cnt == 1:
-        teams += 1
+            cnt.append(lines[i][j])
+    if len(cnt) == 2 and check(cnt):
+        teams.append(cnt)
     
 for i in range(3):
-    cnt = 0
     point = lines[0][i]
+    cnt = []
+    cnt.append(point)
     for j in range(3):
         if lines[j][i] != point:
-            cnt += 1
-    if cnt == 1:
-        teams += 1
+            cnt.append(lines[j][i])
+    if len(cnt) == 2 and check(cnt):
+        teams.append(cnt)
 
-cnt = 0
 point = lines[0][0]
+cnt = []
+cnt.append(point)
 for i in range(3):
     if lines[i][i] != point:
-        cnt += 1
-if cnt == 1:
-    teams += 1
+        cnt.append(lines[i][i])
+if len(cnt) == 2 and check(cnt):
+    teams.append(cnt)
 
-cnt = 0
 point = lines[0][2]
+cnt = []
+cnt.append(point)
 for i in range(3):
     if lines[i][2 - i] != point:
-        cnt += 1
-if cnt == 1:
-    teams += 1
+        cnt.append(lines[i][2 - i])
+if len(cnt) == 2 and check(cnt):
+    teams.append(cnt)
 
-print(teams)
+print(len(teams))

ab006019b316414b7b683f1d58e5719f6aefd943
hanseul37 <hanseul37@daum.net>
2024-08-06 16:53:42 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240806/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 240806/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -2,19 +2,21 @@ n = int(input())
 word = input()
 
 max_length = 0
-for i in range(1, n + 1):
+
+# 연속된 문자로 구성된 부분 문자열인지 확인하는 함수
+def is_consecutive(sub):
+    for i in range(len(sub) - 1):
+        if ord(sub[i]) + 1 != ord(sub[i + 1]):
+            return False
+    return True
+
+# 가능한 모든 부분 문자열 길이에 대해 반복
+for i in range(1, n):
     for j in range(n - i):
-        flag = 1
-        first = []
-        for k in range(j, j + i):
-            if not (ord(word[k]) + 1 == ord(word[k + 1])):
-                flag = 0
-                break
-            first.append(word[k])
-        if flag == 1:
-            first_str = ''.join(first)
+        first = word[j:j + i]
+        if is_consecutive(first):
             second = word[j + i:]
-            if first_str in second:
+            if first in second:
                 max_length = i
 
 print(max_length + 1)

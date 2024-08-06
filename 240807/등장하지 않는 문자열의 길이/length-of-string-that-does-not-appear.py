1de935ba4405fb9a31a64ef0e03e26ce43676b4a
hanseul37 <hanseul37@daum.net>
2024-08-07 07:56:12 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240807/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 240807/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -4,18 +4,13 @@ word = input()
 max_length = 0
 for i in range(1, n + 1):
     for j in range(n - i):
-        flag = 1
         first = []
         for k in range(j, j + i):
-            if i != 1 and ord(word[k]) + 1 != ord(word[k + 1]):
-                flag = 0
-                break
             first.append(word[k])
-        if flag == 1:
-            first_str = ''.join(first)
-            second = word[j + i:]
-            if first_str in second:
-                max_length = i
-                break
+        first_str = ''.join(first)
+        second = word[j + i:]
+        if first_str in second:
+            max_length = i
+            break
 
 print(max_length + 1)

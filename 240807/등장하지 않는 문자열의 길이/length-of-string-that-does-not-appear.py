9327305c5abe512dc566cfc1df7491a7903686b9
hanseul37 <hanseul37@daum.net>
2024-08-07 07:52:54 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240807/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 240807/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -6,7 +6,7 @@ for i in range(1, n + 1):
     for j in range(n - i):
         flag = 1
         first = []
-        for k in range(j - 1, j + i - 1):
+        for k in range(j, j + i):
             if i != 1 and ord(word[k]) + 1 != ord(word[k + 1]):
                 flag = 0
                 break

940d930419d89993cabd265d49e4927cc3165724
hanseul37 <hanseul37@daum.net>
2024-08-07 15:59:17 +0900

    Title: 가장 작은 x 찾기, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240807/가장 작은 x 찾기/find-smallest-x.py
@@ -0,0 +1,16 @@
+n = int(input())
+a_arr, b_arr = [], []
+for _ in range(n):
+    a, b = list(map(int, input().split()))
+    a_arr.append(a)
+    b_arr.append(b)
+
+for i in range(1, 11):
+    flag = 0
+    for j in range(n):
+        if not (a_arr[j] <= i * (2 ** (j + 1)) <= b_arr[j]):
+            flag = 1
+            break
+    if flag == 0:
+        print(i)
+        break

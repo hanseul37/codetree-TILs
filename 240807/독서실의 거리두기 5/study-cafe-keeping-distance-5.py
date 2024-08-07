7df77eeb4f314bd6ef92104c03084fa08bbff77e
hanseul37 <hanseul37@daum.net>
2024-08-07 13:39:27 +0900

    Title: 독서실의 거리두기 5, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240807/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 240807/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -1,19 +1,19 @@
 n = int(input())
 seats = input()
-min_dist = n
+min_dist = 0
 for i in range(n):
     if seats[i] == '0':
         copy_seats = list(seats)
         copy_seats[i] = '1'
-        dist = 0
+        dist = n
         cnt = 0
-        for j in range(n):
+        for j in range(1, n):
             if copy_seats[j] == '0':
                 cnt += 1
             else:
-                dist = max(dist, cnt)
+                dist = min(dist, cnt)
                 cnt = 0
             if j + 1 == n:
-                dist = max(dist, cnt)
-        min_dist = min(min_dist, dist)
-print(min_dist - 1)
+                dist = min(dist, cnt)
+        min_dist = max(min_dist, dist)
+print(min_dist + 1)

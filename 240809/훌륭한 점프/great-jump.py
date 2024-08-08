b5eb31d06bb33bb3b253dd72e53b8588b049b77a
hanseul37 <hanseul37@daum.net>
2024-08-09 08:38:39 +0900

    Title: 훌륭한 점프, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240809/훌륭한 점프/great-jump.py
@@ -0,0 +1,18 @@
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+point = 0
+values = []
+while point < n - 1 :
+    if point + k >= n - 1:
+        values.append(arr[n - 1])
+        break
+    else:
+        val = 100
+        for i in range(point + 1, point + k + 1):
+            if arr[i] < val:
+                val = arr[i]
+                point = i
+        values.append(arr[point])
+values.sort()
+print(values[-1])

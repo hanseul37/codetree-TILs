718cd5910be7695c9e989e8a78a07911565ed2af
hanseul37 <hanseul37@daum.net>
2024-08-09 08:58:25 +0900

    Title: 훌륭한 점프, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240809/훌륭한 점프/great-jump.py
+++ 240809/훌륭한 점프/great-jump.py
@@ -3,15 +3,11 @@ arr = list(map(int, input().split()))
 
 point = 0
 values = arr[0]
-while point < n - 1 :
-    if point + k >= n - 1:
-        values = max(arr[n - 1], values)
-        break
-    else:
-        val = 100
-        for i in range(point + 1, point + k + 1):
-            if arr[i] < val:
-                val = arr[i]
-                point = i
-        values = max(arr[point], values)
+
+while point < n - 1:
+    next_point = min(point + k, n - 1)
+    min_val = min(arr[point + 1:next_point + 1])
+    point = arr.index(min_val, point + 1, next_point + 1)
+    values = max(values, arr[point])
+
 print(values)

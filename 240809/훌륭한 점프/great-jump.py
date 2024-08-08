daefe4f4f1081ee40ba223a4269e4621933e298e
hanseul37 <hanseul37@daum.net>
2024-08-09 08:53:27 +0900

    Title: 훌륭한 점프, Time: 5041ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

--- 240809/훌륭한 점프/great-jump.py
+++ 240809/훌륭한 점프/great-jump.py
@@ -3,21 +3,15 @@ arr = list(map(int, input().split()))
 
 point = 0
 values = arr[0]
-current_min = arr[0]
-
-while point < n - 1:
-    # 최대 k만큼 전진하면서 최소값 갱신
-    for i in range(point + 1, min(point + k + 1, n)):
-        if arr[i] < current_min:
-            current_min = arr[i]
-            point = i
-
-    # 현재 최소값과 비교하여 최대값 갱신
-    values = max(values, current_min)
-    
-    # 목적지 도달하면 루프 종료
+while point < n - 1 :
     if point + k >= n - 1:
-        values = max(values, min(arr[point + 1:]))
+        values = max(arr[n - 1], values)
         break
-
+    else:
+        val = 100
+        for i in range(point + 1, point + k + 1):
+            if arr[i] < val:
+                val = arr[i]
+                point = i
+        values = max(arr[point], values)
 print(values)

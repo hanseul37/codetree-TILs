4c6b80d614c3f3b3b7cc86bc871cd8c86d494c64
hanseul37 <hanseul37@daum.net>
2024-09-13 17:41:22 +0900

    Title: 이상한 폭탄 3, Time: 136ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240913/이상한 폭탄 3/strange-bomb-3.py
@@ -0,0 +1,27 @@
+n, k = map(int, input().split())
+arr = []
+for i in range(n):
+    arr.append(int(input()))
+
+max_cnt, bomb = 0, 0
+for i in range(1000001):
+    cnt, point = 0, 0
+    while point < n:
+        if arr[point] == i:
+            if point + k < n:
+                for j in range(point + 1, point + k + 1):
+                    if arr[j] == i:
+                        cnt += 1
+                        point = j - 1
+                        break
+            else:
+                for j in range(point + 1, n):
+                    if arr[j] == i:
+                        cnt += 1
+                        point = j - 1
+                        break
+        point += 1
+    if max_cnt < cnt:
+        max_cnt = cnt
+        bomb = i
+print(bomb)

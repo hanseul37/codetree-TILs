8cd4bdba6b36212ef43c613bf947a1df53670865
hanseul37 <hanseul37@daum.net>
2024-09-14 15:10:54 +0900

    Title: 최대 H 점수 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240914/최대 H 점수 2/maximum-h-score-2.py
@@ -0,0 +1,16 @@
+n, l = map(int, input().split()) 
+arr1 = list(map(int, input().split()))
+arr2 = [x + 1 for x in arr1]
+
+def check(arr, h):
+    cnt = 0
+    for elem in arr:
+        if elem >= h:
+            cnt += 1
+    return cnt
+
+h = 0
+for i in range(1, 101):
+    if check(arr1, i) > i and check(arr1, i) - check(arr2, i) <= l:
+        h = i
+print(h)

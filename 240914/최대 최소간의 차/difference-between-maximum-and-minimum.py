988bcd86438f809ed3e888d7a7ca95fb112b71f8
hanseul37 <hanseul37@daum.net>
2024-09-14 19:54:36 +0900

    Title: 최대 최소간의 차, Time: 126ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240914/최대 최소간의 차/difference-between-maximum-and-minimum.py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+min_diff = 10000
+for i in range(min(arr), max(arr)):
+    diff = 0
+    for j in range(n):
+        if arr[j] < i:
+            diff += i - arr[j]
+        elif arr[j] > i + k:
+            diff += arr[j] - i - k
+    min_diff = min(min_diff, diff)  
+print(min_diff)

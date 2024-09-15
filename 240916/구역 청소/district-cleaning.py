eb787f309dede03e35bdacf6eacb22d1f7ee4e9c
hanseul37 <hanseul37@daum.net>
2024-09-16 00:20:25 +0900

    Title: 구역 청소, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240916/구역 청소/district-cleaning.py
@@ -0,0 +1,14 @@
+a, b = map(int, input().split())
+c, d = map(int, input().split())
+if c < b < d:
+    if c < a < d:
+        print(d - c)
+    else:
+        print(b + d - a - c - (b - c))
+elif a < d < b:
+    if a < c < b:
+        print(b - a)
+    else:
+        print(b + d - a - c - (d - a))
+else:
+    print(b + d - a - c)

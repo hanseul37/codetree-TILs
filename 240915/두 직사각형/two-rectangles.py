2887719ab7d66dfaf6caab7d21c1fdd91ce68658
hanseul37 <hanseul37@daum.net>
2024-09-15 22:38:26 +0900

    Title: 두 직사각형, Time: 154ms, Memory: 24MB, Status: Passed - CodeTree

--- 240915/두 직사각형/two-rectangles.py
+++ 240915/두 직사각형/two-rectangles.py
@@ -1,9 +1,6 @@
 x1, y1, x2, y2 = map(int, input().split())
 a1, b1, a2, b2 = map(int, input().split())
-if a1 <= x1 <= a2 or a1 <= x2 <= a2: 
-    if b1 <= y1 <= b2 or b1 <= y2 <= b2:
-        print('overlapping')
-    else:
-        print('nonoverlapping')
-else:
+if a1 > x2 or x1 > a2 or b1 > y2 or y1 > b2:
     print('nonoverlapping')
+else:
+    print('overlapping')

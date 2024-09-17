ef438865530b19a9e829e9a4aa9d73974898e184
hanseul37 <hanseul37@daum.net>
2024-09-17 19:42:56 +0900

    Title: 독서실의 거리두기 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240917/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 240917/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -4,7 +4,14 @@ max_cnt = 0
 cnt = 0
 end = 1
 for i in range(1, len(arr)):
-    if arr[i] == '0':
+    if i == n - 1:
+        if arr[i] == '0':
+            cnt += 1
+        if max_cnt < cnt:
+            max_cnt = cnt
+            end = i
+        cnt = 0
+    elif arr[i] == '0':
         cnt += 1
     else:
         if max_cnt < cnt:
@@ -17,7 +24,12 @@ arr[end - max_cnt // 2 - 1] = '1'
 min_cnt = n
 cnt = 0
 for i in range(1, len(arr)):
-    if arr[i] == '0':
+    if i == n - 1:
+        if arr[i] == '0':
+            cnt += 1
+        min_cnt = min(max_cnt, cnt)
+        cnt = 0
+    elif arr[i] == '0':
         cnt += 1
     else:
         min_cnt = min(min_cnt, cnt)

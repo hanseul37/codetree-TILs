77ad2bd4a7e6fdc0cd1e2fea4318930f6cff68bb
hanseul37 <hanseul37@daum.net>
2024-09-17 18:52:51 +0900

    Title: 연속된 숫자 만들기 3, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240917/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
+++ 240917/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
@@ -1,9 +1,2 @@
 a, b, c = map(int, input().split())
-if a + 1 == b and b + 1 == c:
-    print(0)
-elif a + 2 == b or b + 2 == c:
-    print(1)
-elif a + 3 == b or b + 3 == c:
-    print(2)
-else:
-    print(3)
+print(max(b - a, c - b) - 1)

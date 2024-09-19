d891358297d82bcb15062d66a9fcae33a033c248
hanseul37 <hanseul37@daum.net>
2024-09-20 01:54:46 +0900

    Title: 순위 경쟁2, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240920/순위 경쟁2/ranking-competition2.py
@@ -0,0 +1,21 @@
+n = int(input())
+arr = [0, 0]
+point = 'AB'
+cnt = 0
+for i in range(n):
+    c, s = input().split()
+    if c == 'A':
+        arr[0] += int(s)
+    else: 
+        arr[1] += int(s)
+        
+    if arr[0] == arr[1] and point != 'AB':
+        cnt += 1
+        point = 'AB'
+    elif arr[0] > arr[1] and point != 'A':
+        cnt += 1
+        point = 'A'
+    elif arr[0] < arr[1] and point != 'B':
+        cnt += 1
+        point = 'B'
+print(cnt)

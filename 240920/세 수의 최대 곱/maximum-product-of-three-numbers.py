3bae24b9049a3a614ff1558e0ea8e8f18eabf0c1
hanseul37 <hanseul37@daum.net>
2024-09-20 16:49:54 +0900

    Title: 세 수의 최대 곱, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240920/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 240920/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -2,26 +2,31 @@ n = int(input())
 arr = list(map(int, input().split()))
 cnt = 0
 max_value = -1000
-for i in range(n - 2):
-    if arr[i] == 0:
-        continue
-    elif arr[i] < 0:
-        cnt += 1
-
-    for j in range(i + 1, n - 1):
-        if arr[j] == 0:
+if max(arr) < 0 or min(arr) > 0:
+    copy_arr = sorted(arr)
+    max_value = copy_arr[-1] * copy_arr[-2] * copy_arr[-3]
+else:
+    for i in range(n - 2):
+        if arr[i] == 0:
             continue
         elif arr[i] < 0:
             cnt += 1
 
-        for k in range(j + 1, n):
-            if arr[k] == 0:
+        for j in range(i + 1, n - 1):
+            if arr[j] == 0:
                 continue
-            elif arr[k] < 0 and (cnt == 0 or cnt == 2):
-                continue
-            elif arr[k] > 0 and cnt == 1:
-                continue
-            else:
-                max_value = max(max_value, arr[i] * arr[j] * arr[k])
-max_value = max(max_value, 0)
+            elif arr[i] < 0:
+                cnt += 1
+
+            for k in range(j + 1, n):
+                if arr[k] == 0:
+                    continue
+                elif arr[k] < 0 and (cnt == 0 or cnt == 2):
+                    continue
+                elif arr[k] > 0 and cnt == 1:
+                    continue
+                else:
+                    max_value = max(max_value, arr[i] * arr[j] * arr[k])
+    max_value = max(max_value, 0)
+
 print(max_value)

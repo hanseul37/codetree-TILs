d61d9e448dfec4c08e1d3e3056ccf52ecb440fc3
hanseul37 <hanseul37@daum.net>
2024-11-15 14:49:43 +0900

    Title: 아름다운 수, Time: 130ms, Memory: 22MB, Status: Runtime Error - CodeTree

---
+++ 241115/아름다운 수/beautiful-number.py
@@ -0,0 +1,28 @@
+n = int(input())
+arr = []
+count = 0
+
+def check(array):
+    point = 0
+    while point < len(array):
+        cnt = 1
+        while point + cnt < len(array) and array[point] == array[point + cnt]:
+            cnt += 1
+        if array[point] != cnt:
+            return False
+        point += cnt
+    return True
+        
+def choose(num):
+    global count
+    if num == n and check(arr):
+        count += 1
+        return
+        
+    for i in range(1, 5):
+        arr.append(i)
+        choose(num + 1)
+        arr.pop()
+
+choose(1)
+print(count)

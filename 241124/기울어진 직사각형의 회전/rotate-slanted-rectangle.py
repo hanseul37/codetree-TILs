247458af4b75dcf38250d93b2bcb77f68edc0849
hanseul37 <hanseul37@daum.net>
2024-11-24 23:37:30 +0900

    Title: 기울어진 직사각형의 회전, Time: 77ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241124/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -0,0 +1,61 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+r, c, m1, m2, m3, m4, dir = map(int, input().split())
+square = []
+dx, dy = [[1, -1, -1, 1], [-1, 1, 1, -1]], [[-1, -1, 1, 1], [-1, -1, 1, 1]]
+r -= 1
+c -= 1
+
+line = []
+for i in range(m1):
+    line.append(arr[r][c])
+    r += dy[dir][0]
+    c += dx[dir][0]
+square.append(line)
+temp1 = arr[r - dy[dir][0]][c - dx[dir][0]]
+
+line = []
+for i in range(m2):
+    line.append(arr[r][c])
+    r += dy[dir][1]
+    c += dx[dir][1]
+square.append(line)
+temp2 = arr[r - dy[dir][1]][c - dx[dir][1]]
+
+line = []
+for i in range(m3):
+    line.append(arr[r][c])
+    r += dy[dir][2]
+    c += dx[dir][2]
+square.append(line)
+temp3 = arr[r - dy[dir][2]][c - dx[dir][2]]
+
+line = []
+for i in range(m4):
+    line.append(arr[r][c])
+    r += dy[dir][3]
+    c += dx[dir][3]
+square.append(line)
+temp4 = arr[r - dy[dir][3]][c - dx[dir][3]]
+
+for i in range(m1):
+    r += dy[dir][0]
+    c += dx[dir][0]
+    arr[r][c] = square[0][i]
+for i in range(m2):
+    r += dy[dir][1]
+    c += dx[dir][1]
+    arr[r][c] = square[1][i]
+for i in range(m3):
+    r += dy[dir][2]
+    c += dx[dir][2]
+    arr[r][c] = square[2][i]
+for i in range(m4):
+    r += dy[dir][3]
+    c += dx[dir][3]
+    arr[r][c] = square[3][i]
+
+for i in range(n):
+    for j in range(n):
+        print(arr[i][j], end=' ')
+    print()

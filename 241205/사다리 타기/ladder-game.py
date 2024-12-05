9d5adf2eec9d89afef0ec36d799fa2b5be4ce322
hanseul37 <hanseul37@daum.net>
2024-12-05 20:21:00 +0900

    Title: 사다리 타기, Time: 94ms, Memory: 19MB, Status: Passed - CodeTree

--- 241205/사다리 타기/ladder-game.py
+++ 241205/사다리 타기/ladder-game.py
@@ -1,16 +1,20 @@
+from itertools import combinations
+
+def simulate_ladder(ladder):
+    answer = [i for i in range(n)]
+    for a, b in sorted(ladder, key=lambda x: x[1]):
+        answer[a - 1], answer[a] = answer[a], answer[a - 1]
+    return answer
+
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(m)]
-answer = [i for i in range(n)]
 
-arr = sorted(arr, key=lambda x: x[1])
-for i in range(m):
-    temp = answer[arr[i][0] - 1]
-    answer[arr[i][0] - 1] = answer[arr[i][0]]
-    answer[arr[i][0]] = temp
-    
-cnt = 0
-for i in range(n):
-    if answer[i] - i > 0:
-        cnt += answer[i] - i
+result = simulate_ladder(arr)
+
+cnt = m
+for line in range(m + 1):
+    for subset in combinations(arr, line):
+        if simulate_ladder(subset) == result:
+            cnt = min(cnt, line)
 
 print(cnt)

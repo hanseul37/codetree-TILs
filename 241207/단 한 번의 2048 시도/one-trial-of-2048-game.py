c7b4be96a269fff07d8a1ff75e083a089ede1775
hanseul37 <hanseul37@daum.net>
2024-12-07 18:26:24 +0900

    Title: 단 한 번의 2048 시도, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241207/단 한 번의 2048 시도/one-trial-of-2048-game.py
@@ -0,0 +1,87 @@
+arr = [list(map(int, input().split())) for _ in range(4)]
+direction = input()
+
+if direction == 'L':
+    for i in range(4):
+        non_zero, zero = [], []
+        for j in range(4):
+            if arr[i][j] == 0:
+                zero.append(arr[i][j])
+            else:
+                non_zero.append(arr[i][j])
+        for j in range(len(non_zero)):
+            arr[i][j] = non_zero[j]
+        for j in range(len(non_zero), 4):
+            arr[i][j] = 0
+    for i in range(4):
+        for j in range(3):
+            if arr[i][j] == arr[i][j + 1]:
+                arr[i][j] = arr[i][j] * 2
+                for k in range(j + 1, 3):
+                    arr[i][k] = arr[i][k + 1]
+                arr[i][-1] = 0
+    
+elif direction == 'R':
+    for i in range(4):
+        non_zero, zero = [], []
+        for j in range(4):
+            if arr[i][j] == 0:
+                zero.append(arr[i][j])
+            else:
+                non_zero.append(arr[i][j])
+        for j in range(len(zero)):
+            arr[i][j] = 0
+        for j in range(len(zero), 4):
+            arr[i][j] = non_zero[j - len(zero)]
+    for i in range(4):
+        for j in range(3, 0, -1):
+            if arr[i][j] == arr[i][j - 1]:
+                arr[i][j] = arr[i][j] * 2
+                for k in range(j - 1, 0, -1):
+                    arr[i][k] = arr[i][k - 1]
+                arr[i][0] = 0
+
+elif direction == 'U':
+    for i in range(4):
+        non_zero, zero = [], []
+        for j in range(4):
+            if arr[j][i] == 0:
+                zero.append(arr[i][j])
+            else:
+                non_zero.append(arr[i][j])
+        for j in range(len(non_zero)):
+            arr[j][i] = non_zero[j]
+        for j in range(len(non_zero), 4):
+            arr[j][i] = 0
+    for i in range(4):
+        for j in range(3):
+            if arr[j][i] == arr[j + 1][i]:
+                arr[j][i] = arr[j][i] * 2
+                for k in range(j + 1, 3):
+                    arr[k][i] = arr[k + 1][i]
+                arr[-1][i] = 0
+
+elif direction == 'D':
+    for i in range(4):
+        non_zero, zero = [], []
+        for j in range(4):
+            if arr[j][i] == 0:
+                zero.append(arr[i][j])
+            else:
+                non_zero.append(arr[i][j])
+        for j in range(len(zero)):
+            arr[j][i] = 0
+        for j in range(len(zero), 4):
+            arr[j][i] = non_zero[j - len(zero)]
+    for i in range(4):
+        for j in range(3, 0, -1):
+            if arr[j][i] == arr[j - 1][i]:
+                arr[j][i] = arr[j][i] * 2
+                for k in range(j - 1, 0, -1):
+                    arr[k][i] = arr[k - 1][i]
+                arr[0][i] = 0
+
+for i in range(4):
+    for j in range(4):
+        print(arr[i][j], end=' ')
+    print()

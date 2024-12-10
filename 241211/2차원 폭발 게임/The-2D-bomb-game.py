394520ed1a807895872a317831b68ab4ba5b0b2b
hanseul37 <hanseul37@daum.net>
2024-12-11 00:36:57 +0900

    Title: 2차원 폭발 게임, Time: 1047ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241211/2차원 폭발 게임/The-2D-bomb-game.py
+++ 241211/2차원 폭발 게임/The-2D-bomb-game.py
@@ -1,7 +1,7 @@
 n, m, k = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-for _ in range(k + 1):
+for _ in range(k):
     for i in range(n):
         point = arr[0][i]
         cnt = 1
@@ -12,21 +12,6 @@ for _ in range(k + 1):
                 continue
             elif arr[j][i] == point:
                 cnt += 1
-                if j + 1 == n and cnt >= m:
-                    for k in range(n - cnt, n):
-                        arr[k][i] = 0
-                    for a in range(n):
-                        non_zero = []
-                        for b in range(n):
-                            if arr[b][a] != 0:
-                                non_zero.append(arr[b][a])
-                        for b in range(n - len(non_zero)):
-                            arr[b][a] = 0
-                        for b in range(n - len(non_zero), n):
-                            arr[b][a] = non_zero[b - n + len(non_zero)]
-                    j = 0
-                    point = arr[j][i]
-                    cnt = 1
             else:
                 if cnt >= m:
                     for k in range(j - cnt, j):
@@ -44,6 +29,9 @@ for _ in range(k + 1):
                 point = arr[j][i]
                 cnt = 1
             j += 1
+        if cnt >= m:
+            for k in range(n - cnt, n):
+                arr[k][i] = 0
     
     for i in range(n):
         non_zero = []
@@ -70,6 +58,37 @@ for _ in range(k + 1):
             arr[j][i] = 0
         for j in range(n - len(non_zero), n):
             arr[j][i] = non_zero[j - n + len(non_zero)]
+
+    for i in range(n):
+        point = arr[0][i]
+        cnt = 1
+        j = 1
+        while j < n:
+            if arr[j][i] == 0:
+                j += 1
+                continue
+            elif arr[j][i] == point:
+                cnt += 1
+            else:
+                if cnt >= m:
+                    for k in range(j - cnt, j):
+                        arr[k][i] = 0
+                    for a in range(n):
+                        non_zero = []
+                        for b in range(n):
+                            if arr[b][a] != 0:
+                                non_zero.append(arr[b][a])
+                        for b in range(n - len(non_zero)):
+                            arr[b][a] = 0
+                        for b in range(n - len(non_zero), n):
+                            arr[b][a] = non_zero[b - n + len(non_zero)]
+                    j = 0
+                point = arr[j][i]
+                cnt = 1
+            j += 1
+        if cnt >= m:
+            for k in range(n - cnt, n):
+                arr[k][i] = 0
     
 cnt = 0
 for i in range(n):

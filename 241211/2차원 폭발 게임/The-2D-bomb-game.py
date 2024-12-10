c35b88222df79752158cb3a81e95283de64aee0d
hanseul37 <hanseul37@daum.net>
2024-12-11 00:16:38 +0900

    Title: 2차원 폭발 게임, Time: 452ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241211/2차원 폭발 게임/The-2D-bomb-game.py
@@ -0,0 +1,58 @@
+n, m, k = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+for _ in range(k):
+    for i in range(n):
+        point = arr[0][i]
+        cnt = 1
+        j = 1
+        while j < n:
+            if arr[j][i] == 0:
+                j += 1
+                continue
+            elif arr[j][i] == point:
+                cnt += 1
+            else:
+                if cnt >= m:
+                    for k in range(j - cnt, j):
+                        arr[k][i] = 0
+                    j = 0
+                point = arr[j][i]
+                cnt = 1
+            j += 1
+        if cnt >= m:
+            for k in range(n - cnt, n):
+                arr[k][i] = 0
+    
+    for i in range(n):
+        non_zero = []
+        for j in range(n):
+            if arr[j][i] != 0:
+                non_zero.append(arr[j][i])
+        for j in range(n - len(non_zero)):
+            arr[j][i] = 0
+        for j in range(n - len(non_zero), n):
+            arr[j][i] = non_zero[j - n + len(non_zero)]
+    
+    new_arr = [[0] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            new_arr[i][j] = arr[n - j - 1][i]
+    arr = new_arr
+
+    for i in range(n):
+        non_zero = []
+        for j in range(n):
+            if arr[j][i] != 0:
+                non_zero.append(arr[j][i])
+        for j in range(n - len(non_zero)):
+            arr[j][i] = 0
+        for j in range(n - len(non_zero), n):
+            arr[j][i] = non_zero[j - n + len(non_zero)]
+    
+cnt = 0
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] != 0:
+            cnt += 1
+print(cnt)

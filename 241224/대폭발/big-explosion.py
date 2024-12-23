3403c1700d5c289443677fb0aef4942ae2cad841
hanseul37 <hanseul37@daum.net>
2024-12-24 07:53:18 +0900

    Title: 대폭발, Time: 95ms, Memory: 19MB, Status: Passed - CodeTree

--- 241224/대폭발/big-explosion.py
+++ 241224/대폭발/big-explosion.py
@@ -4,19 +4,19 @@ n, m, r, c = map(int, input().split())
 arr = [[0] * n for _ in range(n)]
 arr[r - 1][c - 1] = 1
 
-for i in range(m):
+for i in range(1, m + 1):
     copy_arr = deepcopy(arr)
     for a in range(n):
         for b in range(n):
             if arr[a][b] == 1:
-                if a - (i + 1) >= 0:
-                    copy_arr[a - (i + 1)][b] = 1
-                if b - (i + 1) >= 0:
-                    copy_arr[a][b - (i + 1)] = 1
-                if a + (i + 1) < n:
-                    copy_arr[a + (i + 1)][b] = 1
-                if b + (i + 1) < n:
-                    copy_arr[a][b + (i + 1)] = 1
+                if a - (2 ** (i - 1)) >= 0:
+                    copy_arr[a - (2 ** (i - 1))][b] = 1
+                if b - (2 ** (i - 1)) >= 0:
+                    copy_arr[a][b - (2 ** (i - 1))] = 1
+                if a + (2 ** (i - 1)) < n:
+                    copy_arr[a + (2 ** (i - 1))][b] = 1
+                if b + (2 ** (i - 1)) < n:
+                    copy_arr[a][b + (2 ** (i - 1))] = 1
     arr = copy_arr
 
 cnt = 0

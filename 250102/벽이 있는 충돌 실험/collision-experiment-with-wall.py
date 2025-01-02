0b9d5c5f48387527a68a9ad82f477358ac7ed234
hanseul37 <hanseul37@daum.net>
2025-01-02 22:19:11 +0900

    Title: 벽이 있는 충돌 실험, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250102/벽이 있는 충돌 실험/collision-experiment-with-wall.py
+++ 250102/벽이 있는 충돌 실험/collision-experiment-with-wall.py
@@ -1,12 +1,11 @@
 t = int(input())
 for _ in range(t):
     n, m = map(int, input().split())
-    beads = [input() for _ in range(m)]
+    beads = [input().split() for _ in range(m)]
     for i in range(m):
-        beads[i][0] = int(beads[i][0]) - 1
-        beads[i][1] = int(beads[i][1]) - 1
+        beads[i] = [int(beads[i][0]) - 1, int(beads[i][1]) - 1, beads[i][2]]
     for _ in range(2 * n):
-        new_beads = [[-1, -1, ''] * len(beads)]
+        new_beads = [[-1, -1, ''] for _ in range(len(beads))]
         for i in range(len(beads)):
             if beads[i][2] == 'U':
                 if beads[i][0] != 0:
@@ -14,9 +13,30 @@ for _ in range(t):
                 else:
                     new_beads[i] = [beads[i][0] + 1, beads[i][1], 'D']
             elif beads[i][2] == 'D':
+                if beads[i][0] != n - 1:
+                    new_beads[i] = [beads[i][0] + 1, beads[i][1], beads[i][2]]
+                else:
+                    new_beads[i] = [beads[i][0] - 1, beads[i][1], 'U']
             elif beads[i][2] == 'R':
+                if beads[i][1] != n - 1:
+                    new_beads[i] = [beads[i][0], beads[i][1] + 1, beads[i][2]]
+                else:
+                    new_beads[i] = [beads[i][0], beads[i][1] - 1, 'L']
             elif beads[i][2] == 'L':
+                if beads[i][1] != 0:
+                    new_beads[i] = [beads[i][0], beads[i][1] - 1, beads[i][2]]
+                else:
+                    new_beads[i] = [beads[i][0], beads[i][1] + 1, 'R']
             
-        for i in range(len(new_beads)):
-            
+        unique_beads = []
+        visited = set()  
+        for bead in new_beads:
+            coord = (bead[0], bead[1])
+            if coord in visited:
+                unique_beads = [b for b in unique_beads if (b[0], b[1]) != coord]
+            else:
+                visited.add(coord)
+                unique_beads.append(bead)
+        beads = unique_beads  
+    print(len(beads))     
 

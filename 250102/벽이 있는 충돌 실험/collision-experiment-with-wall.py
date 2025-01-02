aa1da5f97c337f35062f7906ba4bebf121e60ef8
hanseul37 <hanseul37@daum.net>
2025-01-02 22:26:21 +0900

    Title: 벽이 있는 충돌 실험, Time: 5030ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 250102/벽이 있는 충돌 실험/collision-experiment-with-wall.py
+++ 250102/벽이 있는 충돌 실험/collision-experiment-with-wall.py
@@ -11,22 +11,22 @@ for _ in range(t):
                 if beads[i][0] != 0:
                     new_beads[i] = [beads[i][0] - 1, beads[i][1], beads[i][2]]
                 else:
-                    new_beads[i] = [beads[i][0] + 1, beads[i][1], 'D']
+                    new_beads[i] = [beads[i][0], beads[i][1], 'D']
             elif beads[i][2] == 'D':
                 if beads[i][0] != n - 1:
                     new_beads[i] = [beads[i][0] + 1, beads[i][1], beads[i][2]]
                 else:
-                    new_beads[i] = [beads[i][0] - 1, beads[i][1], 'U']
+                    new_beads[i] = [beads[i][0], beads[i][1], 'U']
             elif beads[i][2] == 'R':
                 if beads[i][1] != n - 1:
                     new_beads[i] = [beads[i][0], beads[i][1] + 1, beads[i][2]]
                 else:
-                    new_beads[i] = [beads[i][0], beads[i][1] - 1, 'L']
+                    new_beads[i] = [beads[i][0], beads[i][1], 'L']
             elif beads[i][2] == 'L':
                 if beads[i][1] != 0:
                     new_beads[i] = [beads[i][0], beads[i][1] - 1, beads[i][2]]
                 else:
-                    new_beads[i] = [beads[i][0], beads[i][1] + 1, 'R']
+                    new_beads[i] = [beads[i][0], beads[i][1], 'R']
             
         unique_beads = []
         visited = set()  

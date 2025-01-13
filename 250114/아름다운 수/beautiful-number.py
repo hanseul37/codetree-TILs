14f92a1d513beca1d30f59a2728f9fbf2275ada3
hanseul37 <hanseul37@daum.net>
2025-01-14 08:06:42 +0900

    Title: 아름다운 수, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250114/아름다운 수/beautiful-number.py
+++ 250114/아름다운 수/beautiful-number.py
@@ -1,24 +1,12 @@
-def is_beautiful_number(num):
-    s = str(num)
+def count_beautiful_numbers(current_length, n):
+    if current_length == n:
+        return 1
+
     cnt = 0
-    while cnt < len(s):
-        target = s[cnt]
-        if not ('1' <= target <= '4'):
-            return False
-        if len(s) - cnt < int(target):
-            return False
-        for _ in range(int(target)):
-            if s[cnt] != target:
-                return False
-            cnt += 1
-    return True
+    for num in range(1, 5):
+        if current_length + num <= n:
+            cnt += count_beautiful_numbers(current_length + num, n)
+    return cnt
 
 n = int(input())
-start = 10 ** (n - 1)
-end = 10 ** n
-beautiful_cnt = 0
-for num in range(start, end):
-    if is_beautiful_number(num):
-        beautiful_cnt += 1
-
-print(beautiful_cnt)
+print(count_beautiful_numbers(0, n))

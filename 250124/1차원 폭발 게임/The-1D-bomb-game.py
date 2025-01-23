760b67af801c3bdfe4f94a7d79fcf673d99aff2b
hanseul37 <hanseul37@daum.net>
2025-01-24 07:33:36 +0900

    Title: 1차원 폭발 게임, Time: 71ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250124/1차원 폭발 게임/The-1D-bomb-game.py
@@ -0,0 +1,25 @@
+n, m = map(int, input().split())
+bombs = [int(input()) for _ in range(n)]
+flag = True
+
+while flag:
+    new_bombs, temp = [], []
+    target = bombs[0]
+    flag = False
+    for i in range(len(bombs)):
+        if bombs[i] != target:
+            target = bombs[i]
+            if len(temp) < m:
+                new_bombs += temp
+            else:
+                flag = True
+            temp = [target] 
+        else:
+            temp.append(bombs[i])
+    if len(temp) < m:
+        new_bombs += temp
+    bombs = new_bombs
+
+print(len(bombs))
+for bomb in bombs:
+    print(bomb)

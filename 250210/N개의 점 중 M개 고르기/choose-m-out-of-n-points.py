adadab994496fa0eba93ed43de1d89f547dd1087
hanseul37 <hanseul37@daum.net>
2025-02-10 08:42:50 +0900

    Title: N개의 점 중 M개 고르기, Time: 219ms, Memory: 23MB, Status: Passed - CodeTree

--- 250210/N개의 점 중 M개 고르기/choose-m-out-of-n-points.py
+++ 250210/N개의 점 중 M개 고르기/choose-m-out-of-n-points.py
@@ -7,10 +7,11 @@ def choice(points, idx):
     global min_dist
 
     if len(points) >= m:
+        dist = 0
         for i in range(m - 1):
             for j in range(i + 1, m):
-                dist = (points[i][0] - points[j][0]) ** 2 + (points[i][1] - points[j][1]) ** 2
-                min_dist = min(min_dist, dist)
+                dist = max((points[i][0] - points[j][0]) ** 2 + (points[i][1] - points[j][1]) ** 2, dist)
+        min_dist = min(min_dist, dist)
 
     for i in range(idx + 1, n):
         points.append(arr[i])

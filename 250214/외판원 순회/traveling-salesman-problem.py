77bb8f33a72b5bd423075caf7b6691aa4d95656b
hanseul37 <hanseul37@daum.net>
2025-02-14 08:39:30 +0900

    Title: 외판원 순회, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250214/외판원 순회/traveling-salesman-problem.py
@@ -0,0 +1,23 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+min_value = 10000 * n * n
+ans = [0]
+
+def move():
+    global min_value
+
+    if len(ans) == n:
+        value = 0
+        for i in range(n - 1):
+            value += arr[ans[i]][ans[i + 1]]
+        value += arr[ans[-1]][0]
+        min_value = min(min_value, value)
+
+    for i in range(1, n):
+        if i not in ans:
+            ans.append(i)
+            move()
+            ans.pop()
+
+move()
+print(min_value)

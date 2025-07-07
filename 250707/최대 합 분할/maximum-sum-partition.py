2bfa2788b68f9cc247c7115e01b7891bbc61651b
hanseul37 <hanseul37@daum.net>
2025-07-07 15:48:06 +0900

    Title: 최대 합 분할, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250707/최대 합 분할/maximum-sum-partition.py
@@ -0,0 +1,18 @@
+n = int(input())
+arr = list(map(int, input().split()))
+total = sum(arr)
+dp = [0] * (total + 1)
+dp[0] = 1
+
+for num in arr:
+    for j in range(total, num - 1, -1):    
+        if dp[j - num]:
+            dp[j] += 1
+
+max_sum = 0
+for i in range(1, total + 1):
+    if dp[i] > 1:
+        a, c = i, total - i * 2
+        if c >= 0 and dp[c]:
+            max_sum = max(max_sum, a)
+print(max_sum)

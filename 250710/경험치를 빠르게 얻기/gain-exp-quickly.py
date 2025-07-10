292023164ab7c30af5e9b347863f74ec6c9b24d8
hanseul37 <hanseul37@daum.net>
2025-07-10 19:41:37 +0900

    Title: 경험치를 빠르게 얻기, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250710/경험치를 빠르게 얻기/gain-exp-quickly.py
+++ 250710/경험치를 빠르게 얻기/gain-exp-quickly.py
@@ -1,18 +1,22 @@
 n, m = map(int, input().split())
 e, t = [], []
+total_t = 0
 for _ in range(n):
     ei, ti = map(int, input().split())
     e.append(ei)
     t.append(ti)
-dp = [-1] * (sum(t) + 1)
+    total_t += ti
+dp = [-1] * (total_t + 1)
 dp[0] = 0
 
 for i in range(n):
-    for j in range(sum(t), t[i] - 1, -1):
+    for j in range(total_t, t[i] - 1, -1):
         if dp[j - t[i]] >= 0:
             dp[j] = max(dp[j], dp[j - t[i]] + e[i])
-    
+
+ans = -1  
 for i in range(len(dp)):
     if dp[i] >= m:
-        print(i)
+        ans = i
         break
+print(ans)

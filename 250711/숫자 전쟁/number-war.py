41a8d6014efc0a1b1c934f9db2a81fedbf885584
hanseul37 <hanseul37@daum.net>
2025-07-11 22:21:30 +0900

    Title: 숫자 전쟁, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250711/숫자 전쟁/number-war.py
+++ 250711/숫자 전쟁/number-war.py
@@ -1,10 +1,13 @@
 n = int(input())
 first = list(map(int, input().split()))
 second = list(map(int, input().split()))
-dp = [[0] * (n + 1) for _ in range(n + 1)]
+dp = [[-1] * (n + 1) for _ in range(n + 1)]
+dp[0][0] = 0
 
 for i in range(n):
     for j in range(n):
+        if dp[i][j] == -1:
+            continue
         if second[j] > first[i]:
             dp[i + 1][j] = max(dp[i + 1][j], dp[i][j])
         elif second[j] < first[i]:
@@ -12,7 +15,7 @@ for i in range(n):
         else:
             dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j])
         dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j])
-        
+
 max_point = 0
 for i in range(n + 1):
     for j in range(n + 1):

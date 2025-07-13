b1ff91ce8e5512307990b30112dedfa45f426038
hanseul37 <hanseul37@daum.net>
2025-07-13 17:26:24 +0900

    Title: 둘 중 하나 잘 고르기, Time: 64ms, Memory: 17MB, Status: Passed - Codetree

--- 250713/둘 중 하나 잘 고르기/choose-one-of-two-points.py
+++ 250713/둘 중 하나 잘 고르기/choose-one-of-two-points.py
@@ -7,9 +7,12 @@ for _ in range(2 * n):
     red.append(r)
     blue.append(b)
 
-dp = [[0] * (n + 1) for _ in range(2 * n + 1)]
+dp = [[-1] * (n + 1) for _ in range(2 * n + 1)]
+dp[0][0] = 0
 for i in range(2 * n):
     for j in range(n + 1):
+        if dp[i][j] == -1:
+            continue
         if j < n:
             dp[i + 1][j + 1] = max(dp[i][j] + red[i], dp[i + 1][j + 1])
         if i - j < n:

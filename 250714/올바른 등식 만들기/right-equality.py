888b8f6c84e4e073da4a143436619d30d0f70df8
hanseul37 <hanseul37@daum.net>
2025-07-14 23:50:17 +0900

    Title: 올바른 등식 만들기, Time: 53ms, Memory: 16MB, Status: Passed - Codetree

--- 250714/올바른 등식 만들기/right-equality.py
+++ 250714/올바른 등식 만들기/right-equality.py
@@ -1,8 +1,8 @@
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 dp = [[0] * 41 for _ in range(n)]
-dp[0][20 + arr[0]] = 1
-dp[0][20 - arr[0]] = 1
+dp[0][20 + arr[0]] += 1
+dp[0][20 - arr[0]] += 1
 
 for i in range(1, n):
     for j in range(41):

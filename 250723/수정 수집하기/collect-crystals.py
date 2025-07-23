37976fc16d012400e2c139563148bd0bdee12383
hanseul37 <hanseul37@daum.net>
2025-07-23 20:03:21 +0900

    Title: 수정 수집하기, Time: 72ms, Memory: 19MB, Status: Wrong Answer - Codetree

--- 250723/수정 수집하기/collect-crystals.py
+++ 250723/수정 수집하기/collect-crystals.py
@@ -2,27 +2,28 @@ n, k = map(int, input().split())
 crystal = input()
 dp = [[[0] * 2 for _ in range(k + 1)] for _ in range(n + 1)]
 
-# 초기 선택 처리: i = 0 에서 시작한 선택은 j 증가 없이 해야 함
-first = 0 if crystal[0] == 'L' else 1
-dp[1][0][first] = 1  # 첫 문자 선택해서 시작
+if crystal[0] == 'L':
+    dp[1][0][0] = 1
+else:
+    dp[1][0][1] = 1
 
-for i in range(1, n):
-    curr = 0 if crystal[i] == 'L' else 1
+for i in range(n):
     for j in range(k + 1):
         for l in range(2):
-            # 그대로 진행 안 하고 넘기는 경우
-            dp[i + 1][j][l] = max(dp[i + 1][j][l], dp[i][j][l])
-            
-            if l == curr:
-                # 같은 타입이면 그대로 선택
-                dp[i + 1][j][curr] = max(dp[i + 1][j][curr], dp[i][j][l] + 1)
+            curr = -1
+            if crystal[i] == 'L':
+                curr = 0
             else:
-                # 다른 타입이면 전환 필요
+                curr = 1              
+            dp[i + 1][j][l] = max(dp[i + 1][j][l], dp[i][j][l])  
+            if l == curr:
+                dp[i + 1][j][curr] = max(dp[i][j][l] + 1, dp[i + 1][j][curr])
+            else:
                 if j < k:
-                    dp[i + 1][j + 1][curr] = max(dp[i + 1][j + 1][curr], dp[i][j][l] + 1)
+                    dp[i + 1][j + 1][curr] = max(dp[i][j][l] + 1, dp[i + 1][j + 1][curr])             
 
 max_crystal = 0
-for j in range(k + 1):
-    for l in range(2):
-        max_crystal = max(max_crystal, dp[n][j][l])
+for i in range(k + 1):
+    for j in range(2):
+        max_crystal = max(dp[-1][i][j], max_crystal)
 print(max_crystal)

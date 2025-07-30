60ae9e7ba5255b99d0171b33f7d82fb4ff820257
hanseul37 <hanseul37@daum.net>
2025-07-30 18:58:48 +0900

    Title: 최장 공통 부분 수열 2, Time: 85ms, Memory: 23MB, Status: Passed - Codetree

--- 250730/최장 공통 부분 수열 2/longest-common-sequence-2.py
+++ 250730/최장 공통 부분 수열 2/longest-common-sequence-2.py
@@ -1,16 +1,24 @@
 a = input()
 b = input()
 n, m = len(a), len(b)
-dp = [[[] for _ in range(m + 1)] for _ in range(n + 1)]
+dp = [[0] * (m + 1) for _ in range(n + 1)]
 
 for i in range(1, n + 1):
     for j in range(1, m + 1):
-        if len(dp[i - 1][j]) < len(dp[i][j - 1]):
-                dp[i][j] = dp[i][j - 1][:]
-        else:
-            dp[i][j] = dp[i - 1][j][:] 
-        
         if a[i - 1] == b[j - 1]:
-            dp[i][j] = dp[i - 1][j - 1][:] + [a[i - 1]]
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
 
-print(''.join(dp[n][m]))
+ans = []
+while n > 0 and m > 0:
+    if a[n - 1] == b[m - 1]:
+        ans.append(a[n - 1])
+        n -= 1
+        m -= 1
+    elif dp[n - 1][m] >= dp[n][m - 1]:
+        n -= 1
+    else:
+        m -= 1
+
+print(''.join(reversed(ans)))

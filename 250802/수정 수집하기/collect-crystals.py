a36883c498994218c943f461e637bd5c2ac589dc
hanseul37 <hanseul37@daum.net>
2025-08-02 16:24:59 +0900

    Title: 수정 수집하기, Time: 77ms, Memory: 19MB, Status: Wrong Answer - Codetree

--- 250802/수정 수집하기/collect-crystals.py
+++ 250802/수정 수집하기/collect-crystals.py
@@ -1,20 +1,25 @@
 n, k = map(int, input().split())
 crystal = input()
-dp = [[[0] * 2 for _ in range(k + 1)] for _ in range(n + 1)]
+dp = [[[-1] * 2 for _ in range(k + 1)] for _ in range(n + 1)]
+
+dp[0][0][0] = 0
+dp[0][0][1] = 0
 
 for i in range(n):
     for j in range(k + 1):
         for l in range(2):
+            if dp[i][j][l] == -1:
+                continue
             if crystal[i] == 'L':
                 curr = 0
             else:
                 curr = 1
+            dp[i + 1][j][l] = max(dp[i + 1][j][l], dp[i][j][l])
             if l == curr:
                 dp[i + 1][j][curr] = max(dp[i][j][l] + 1, dp[i + 1][j][curr])
             else:
                 if j < k:
                     dp[i + 1][j + 1][curr] = max(dp[i][j][l] + 1, dp[i + 1][j + 1][curr])
-            dp[i + 1][j][l] = max(dp[i + 1][j][l], dp[i][j][l])
 
 max_crystal = 0
 for i in range(k + 1):

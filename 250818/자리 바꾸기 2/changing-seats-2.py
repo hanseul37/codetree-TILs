d1e215c6b211c55ae7bbf8dfec517b4349224e31
hanseul37 <hanseul37@daum.net>
2025-08-18 16:49:35 +0900

    Title: 자리 바꾸기 2, Time: 176ms, Memory: 29MB, Status: Passed - Codetree

--- 250818/자리 바꾸기 2/changing-seats-2.py
+++ 250818/자리 바꾸기 2/changing-seats-2.py
@@ -1,17 +1,16 @@
 n, k = map(int, input().split())
 changes = [list(map(int, input().split())) for _ in range(k)]
 human = [set() for _ in range(n)]
-now = []
+now = list(range(n)) 
 
 for i in range(n):
     human[i].add(i)
-    now.append(i)
 
 for i in range(3 * k):
     a, b = changes[i % k][0] - 1, changes[i % k][1] - 1
-    now[a], now[b] = now[b], now[a]
     human[now[a]].add(b)
     human[now[b]].add(a)
+    now[a], now[b] = now[b], now[a]
 
 for i in range(n):
     print(len(human[i]))

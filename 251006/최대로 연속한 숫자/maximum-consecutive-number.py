176fe265c9c21e9bb9f253d81313559671d4084e
hanseul37 <hanseul37@daum.net>
2025-10-06 23:54:57 +0900

    Title: 최대로 연속한 숫자, Time: 938ms, Memory: 40MB, Status: Wrong Answer - Codetree

--- 251006/최대로 연속한 숫자/maximum-consecutive-number.py
+++ 251006/최대로 연속한 숫자/maximum-consecutive-number.py
@@ -2,26 +2,32 @@ from sortedcontainers import SortedList
 
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
-num = SortedList([[0, n]], key= lambda x: x[0])
-point, max_cnt = 0, 0
 
-for i in range(m):
-    idx = num.bisect_left([arr[i], 0]) - 1    
+num = SortedList([[0, n]], key=lambda x: x[0])
+lengths = SortedList([n - 0 + 1])  # 구간 길이들 관리
+
+for x in arr:
+    idx = num.bisect_left([x, 0]) - 1
     l, r = num[idx]
-    if l == arr[i]:
-        num[idx][0] += 1
-    elif r == arr[i]:
-        num[idx][1] -= 1
-    else:
-        num.pop(idx)
-        num.add([l, arr[i] - 1])
-        num.add([arr[i] + 1, r])
+    length = r - l + 1
     
-    if point == l:
-        max_cnt = 0
-        for elem in num:
-            if elem[1] - elem[0] + 1 > max_cnt:
-                max_cnt = elem[1] - elem[0] + 1
-                point = elem[0]
-    print(max_cnt)
-
+    # 기존 구간 제거
+    num.pop(idx)
+    lengths.remove(length)
+    
+    # 세 경우로 분기
+    if l == r == x:
+        pass  # 구간 완전히 사라짐
+    elif l == x:
+        num.add([x + 1, r])
+        lengths.add(r - (x + 1) + 1)
+    elif r == x:
+        num.add([l, x - 1])
+        lengths.add((x - 1) - l + 1)
+    else:
+        num.add([l, x - 1])
+        num.add([x + 1, r])
+        lengths.add((x - 1) - l + 1)
+        lengths.add(r - (x + 1) + 1)
+    
+    print(lengths[-1] if lengths else 0)

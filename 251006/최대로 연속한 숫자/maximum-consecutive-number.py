6a75b233ffbc617f29c1ed7b2e8f216eba2f7d72
hanseul37 <hanseul37@daum.net>
2025-10-06 23:32:20 +0900

    Title: 최대로 연속한 숫자, Time: 87ms, Memory: 19MB, Status: Wrong Answer - Codetree

--- 251006/최대로 연속한 숫자/maximum-consecutive-number.py
+++ 251006/최대로 연속한 숫자/maximum-consecutive-number.py
@@ -2,21 +2,22 @@ from sortedcontainers import SortedList
 
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
-num = SortedList([[0, n]], key= lambda x: x[0])
+num = SortedList([[0, n]], key=lambda x: x[0])
 
-for i in range(m):
-    idx = num.bisect_left([arr[i], 0]) - 1
-    if num[idx][0] == arr[i]:
-        num[idx][0] += 1
-    elif num[idx][1] == arr[i]:
-        num[idx][1] -= 1
-    else:
-        l, r = num[idx]
-        num.pop(idx)
-        num.add([l, arr[i] - 1])
-        num.add([arr[i] + 1, r])
+# 현재 최대 길이
+max_len = n
+
+for x in arr:
+    idx = num.bisect_left([x, 0]) - 1
+    l, r = num[idx]
     
-    max_cnt = 0
-    for elem in num:
-        max_cnt = max(max_cnt, elem[1] - elem[0] + 1)
-    print(max_cnt)
+    # 구간 업데이트
+    num.pop(idx)
+    if l <= x - 1:
+        num.add([l, x - 1])
+    if x + 1 <= r:
+        num.add([x + 1, r])
+    
+    # max_len 갱신
+    max_len = max(max_len, max(r - l for l, r in num))
+    print(max_len)

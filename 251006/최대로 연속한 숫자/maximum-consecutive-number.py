e8ba875861419f24d9856f225cdf5d17ac55f8ce
hanseul37 <hanseul37@daum.net>
2025-10-06 23:59:43 +0900

    Title: 최대로 연속한 숫자, Time: 709ms, Memory: 33MB, Status: Wrong Answer - Codetree

--- 251006/최대로 연속한 숫자/maximum-consecutive-number.py
+++ 251006/최대로 연속한 숫자/maximum-consecutive-number.py
@@ -1,33 +1,35 @@
-from sortedcontainers import SortedList
+from sortedcontainers import SortedList, SortedDict
 
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
-num = SortedList([[0, n]], key=lambda x: x[0])
-lengths = SortedList([n - 0 + 1])  # 구간 길이들 관리
+intervals = SortedList([[0, n]], key=lambda x: x[0])
+lengths = SortedDict()
+lengths[n - 0 + 1] = 1  # 처음 구간 길이
 
 for x in arr:
-    idx = num.bisect_left([x, 0]) - 1
-    l, r = num[idx]
-    length = r - l + 1
-    
-    # 기존 구간 제거
-    num.pop(idx)
-    lengths.remove(length)
-    
-    # 세 경우로 분기
-    if l == r == x:
-        pass  # 구간 완전히 사라짐
-    elif l == x:
-        num.add([x + 1, r])
-        lengths.add(r - (x + 1) + 1)
-    elif r == x:
-        num.add([l, x - 1])
-        lengths.add((x - 1) - l + 1)
-    else:
-        num.add([l, x - 1])
-        num.add([x + 1, r])
-        lengths.add((x - 1) - l + 1)
-        lengths.add(r - (x + 1) + 1)
-    
-    print(lengths[-1] if lengths else 0)
+    idx = intervals.bisect_left([x, 0]) - 1
+    l, r = intervals[idx]
+    intervals.pop(idx)
+
+    # 기존 구간 길이 제거
+    old_len = r - l + 1
+    lengths[old_len] -= 1
+    if lengths[old_len] == 0:
+        del lengths[old_len]
+
+    # 새 구간들 생성
+    new_intervals = []
+    if l < x:
+        new_intervals.append([l, x - 1])
+    if x < r:
+        new_intervals.append([x + 1, r])
+
+    # 추가
+    for nl, nr in new_intervals:
+        intervals.add([nl, nr])
+        new_len = nr - nl + 1
+        lengths[new_len] = lengths.get(new_len, 0) + 1
+
+    # 현재 최댓값 출력
+    print(lengths.peekitem(-1)[0])

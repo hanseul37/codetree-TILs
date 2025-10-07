17071bc332e4cfc3fea3d289a0059d7f6c0f56ac
hanseul37 <hanseul37@daum.net>
2025-10-07 17:11:48 +0900

    Title: 최대로 연속한 숫자, Time: 969ms, Memory: 51MB, Status: Wrong Answer - Codetree

--- 251007/최대로 연속한 숫자/maximum-consecutive-number.py
+++ 251007/최대로 연속한 숫자/maximum-consecutive-number.py
@@ -5,23 +5,28 @@ arr = list(map(int, input().split()))
 num = SortedList([[0, n]], key=lambda x: x[0])
 length = SortedList([[n + 1, 0]], key=lambda x: -x[0])
 
-for i in range(m):
-    idx = num.bisect_left([arr[i], 0]) - 1
+for val in arr:
+    if not num:
+        print(0)
+        continue
+
+    idx = num.bisect_left([val, 0]) - 1
     l, r = num[idx]
     num.pop(idx)
     length.remove([r - l + 1, l])
-    if l == arr[i]:
-        num.add([l + 1, r])
-        length.add([r - l, l + 1])
-    elif r == arr[i]:
-        num.add([l, r - 1])
-        length.add([r - l, l])
+
+    if l == val:
+        if l + 1 <= r:
+            num.add([l + 1, r])
+            length.add([r - l, l + 1])
+    elif r == val:
+        if l <= r - 1:
+            num.add([l, r - 1])
+            length.add([r - l, l])
     else:
-        num.add([l, arr[i] - 1])
-        num.add([arr[i] + 1, r])
-        length.add([arr[i] - l, l])
-        length.add([r - arr[i], arr[i] + 1])
+        num.add([l, val - 1])
+        num.add([val + 1, r])
+        length.add([val - l, l])
+        length.add([r - val, val + 1])
 
     print(length[0][0])
-
-

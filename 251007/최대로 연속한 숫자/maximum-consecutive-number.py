d55124e71c98362b514f47fb071a7b2d35cc3e31
hanseul37 <hanseul37@daum.net>
2025-10-07 17:31:06 +0900

    Title: 최대로 연속한 숫자, Time: 2212ms, Memory: 51MB, Status: Passed - Codetree

--- 251007/최대로 연속한 숫자/maximum-consecutive-number.py
+++ 251007/최대로 연속한 숫자/maximum-consecutive-number.py
@@ -24,12 +24,10 @@ for val in arr:
             num.add([l, r - 1])
             length.add([r - l, l])
     else:
-        if l <= val - 1:
-            num.add([l, val - 1])
-            length.add([val - l, l])
-        if val + 1 <= r:
-            num.add([val + 1, r])
-            length.add([r - val, val + 1])
+        num.add([l, val - 1])
+        num.add([val + 1, r])
+        length.add([val - l, l])
+        length.add([r - val, val + 1])
 
     if length:
         print(length[0][0])

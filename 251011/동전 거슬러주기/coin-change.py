8278a72ce4e4e103a335c5cae774a015141062cc
hanseul37 <hanseul37@daum.net>
2025-10-11 01:14:12 +0900

    Title: 동전 거슬러주기, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

--- 251011/동전 거슬러주기/coin-change.py
+++ 251011/동전 거슬러주기/coin-change.py
@@ -1,11 +1,13 @@
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 dp = [m + 1] * (m + 1)
-for elem in arr:
-    dp[elem] = 1
+dp[0] = 0
 for i in range(1, m + 1):
     for elem in arr:
-        if i > elem:
+        if i >= elem:
             dp[i] = min(dp[i], dp[i - elem] + 1)
 
-print(dp[m])
+if dp[m] == m + 1:
+    print(-1)
+else:
+    print(dp[m])

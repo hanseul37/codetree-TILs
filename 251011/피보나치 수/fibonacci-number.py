ab2b51206299a0e4adcd0d340cf355bb4f0dad8f
hanseul37 <hanseul37@daum.net>
2025-10-11 00:33:26 +0900

    Title: 피보나치 수, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251011/피보나치 수/fibonacci-number.py
+++ 251011/피보나치 수/fibonacci-number.py
@@ -1,6 +1,9 @@
 n = int(input())
-dp = [0] * n
-dp[0], dp[1] = 1, 1
-for i in range(2, n):
-    dp[i] = dp[i - 2] + dp[i - 1]
-print(dp[n - 1])
+if n == 1 or n == 2:
+    print(1)
+else:
+    dp = [0] * n
+    dp[0], dp[1] = 1, 1
+    for i in range(2, n):
+        dp[i] = dp[i - 2] + dp[i - 1]
+    print(dp[n - 1])

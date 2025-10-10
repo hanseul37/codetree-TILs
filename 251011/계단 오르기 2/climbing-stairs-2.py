5f2e37002d0a9e8142ccc7f37ccd58ec7d376f26
hanseul37 <hanseul37@daum.net>
2025-10-11 01:54:33 +0900

    Title: 계단 오르기 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251011/계단 오르기 2/climbing-stairs-2.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [[-1] * 4 for _ in range(n + 1)]
+dp[0][0] = 0
+dp[1][1] = arr[1]
+
+for i in range(2, n + 1):
+    for j in range(4):
+        if dp[i - 2][j] >= 0:
+            dp[i][j] = max(dp[i][j], dp[i - 2][j] + arr[i - 1])
+        if j > 0 and dp[i - 1][j - 1] >= 0:
+            dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + arr[i - 1])
+print(max(dp[n]))

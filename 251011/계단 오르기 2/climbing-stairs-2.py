27d3fdb3934ae3b4ce15289e52750f2d84e4b8dc
hanseul37 <hanseul37@daum.net>
2025-10-11 01:56:17 +0900

    Title: 계단 오르기 2, Time: 57ms, Memory: 17MB, Status: Passed - Codetree

--- 251011/계단 오르기 2/climbing-stairs-2.py
+++ 251011/계단 오르기 2/climbing-stairs-2.py
@@ -2,7 +2,7 @@ n = int(input())
 arr = list(map(int, input().split()))
 dp = [[-1] * 4 for _ in range(n + 1)]
 dp[0][0] = 0
-dp[1][1] = arr[1]
+dp[1][1] = arr[0]
 
 for i in range(2, n + 1):
     for j in range(4):

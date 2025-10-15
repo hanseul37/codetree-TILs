f7d3c4d9836c16b304b6017fc36038db78a88157
hanseul37 <hanseul37@daum.net>
2025-10-15 18:44:27 +0900

    Title: 길이가 무한대인 수평선 트랙, Time: 1456ms, Memory: 27MB, Status: Wrong Answer - Codetree

--- 251015/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.py
+++ 251015/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.py
@@ -1,26 +1,20 @@
 from sortedcontainers import SortedList
 n, t = map(int, input().split())
-track = SortedList(key=lambda x: (-x[0], x[1]))
+track = SortedList(key=lambda x: -x[0])
 for _ in range(n):
     start, speed = map(int, input().split())
     track.add([start, speed])
 
 for _ in range(t):
     last_location, last_speed = 10 ** 9 + 1, 0   
+    new_track = []
     for elem in track:
-        if elem[0] + elem[1] >= last_location:
-            elem[0] = last_location
-            elem[1] = last_speed
-        else:
-            elem[0] += elem[1]
-            last_location = elem[0]
+        if elem[0] + elem[1] < last_location:
+            new_location = elem[0] + elem[1]
+            new_track.append([new_location, elem[1]])
+            last_location = new_location
             last_speed = elem[1]
+    track = new_track
 
-cnt, point = 0, 0
-for location, _ in track:
-    if location != point:
-        cnt += 1
-        point = location
-
-print(cnt)
+print(len(track))
 

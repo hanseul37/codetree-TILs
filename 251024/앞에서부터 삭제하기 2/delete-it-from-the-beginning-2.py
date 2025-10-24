3f100083484aa630a69305cddfff136a066656dd
hanseul37 <hanseul37@daum.net>
2025-10-24 15:38:18 +0900

    Title: 앞에서부터 삭제하기 2, Time: 101ms, Memory: 27MB, Status: Passed - Codetree

--- 251024/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 251024/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -2,12 +2,12 @@ import heapq
 
 n = int(input())
 arr = list(map(int, input().split()))
-max_value = 0
-for i in range(1, n - 1):
-    temp = arr[i:]
-    heapq.heapify(temp)
-    heapq.heappop(temp)
-    max_value = max(sum(temp) / len(temp), max_value)
+max_value, total = 0, arr[-1]
+pq = [arr[-1]]
+for i in range(n - 2, 0, -1):
+    heapq.heappush(pq, arr[i])
+    total += arr[i]
+    max_value = max((total - pq[0]) / (len(pq) - 1), max_value)
 print(f'{max_value:.2f}')
 
 

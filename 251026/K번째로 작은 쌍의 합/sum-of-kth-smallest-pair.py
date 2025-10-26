fbd54d46af524330eecb43dceab70387f263b2ce
hanseul37 <hanseul37@daum.net>
2025-10-26 12:04:34 +0900

    Title: K번째로 작은 쌍의 합, Time: 180ms, Memory: 28MB, Status: Passed - Codetree

--- 251026/K번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
+++ 251026/K번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
@@ -6,12 +6,15 @@ arr2 = list(map(int, input().split()))
 arr1.sort()
 arr2.sort()
 pq = [[arr1[0] + arr2[0], 0, 0]]
+visited = {(0, 0)}
 
 for _ in range(k):
     value, i, j = heapq.heappop(pq)
+    if i + 1 < n and (i + 1, j) not in visited:
+        heapq.heappush(pq, [arr1[i + 1] + arr2[j], i + 1, j])
+        visited.add((i + 1, j))
+    if j + 1 < m and (i, j + 1) not in visited:
+        heapq.heappush(pq, [arr1[i] + arr2[j + 1], i, j + 1])
+        visited.add((i, j + 1))
 
-    if i + 
-
-    
-
-
+print(value)

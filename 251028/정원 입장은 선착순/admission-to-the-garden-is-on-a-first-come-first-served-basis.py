49e16a435a092319b2a2a9ebf59433e444b70fc2
hanseul37 <hanseul37@daum.net>
2025-10-28 09:57:37 +0900

    Title: 정원 입장은 선착순, Time: 730ms, Memory: 36MB, Status: Passed - Codetree

---
+++ 251028/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
@@ -0,0 +1,26 @@
+import heapq
+
+n = int(input())
+pq = []
+for i in range(n):
+    ai, ti = map(int, input().split())
+    heapq.heappush(pq, (ai, i, ti))
+
+ai, idx, ti = heapq.heappop(pq)
+waiting = [(idx, ai, ti)]
+time, max_waiting = ai, 0
+while waiting:
+    idx, ai, ti = heapq.heappop(waiting)
+    max_waiting = max(time - ai, max_waiting)
+    time += ti
+    while pq:
+        if pq[0][0] <= time:
+            wait = heapq.heappop(pq)
+            heapq.heappush(waiting, (wait[1], wait[0], wait[2]))
+        else:
+            break
+    if not waiting and pq:
+        wait = heapq.heappop(pq)
+        heapq.heappush(waiting, (wait[1], wait[0], wait[2]))
+        time = wait[0]
+print(max_waiting)

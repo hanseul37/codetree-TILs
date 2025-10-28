7fd3b0043a1775a65605ecda1e82ce99b800a8bd
hanseul37 <hanseul37@daum.net>
2025-10-28 15:55:37 +0900

    Title: 최솟값 3개, Time: 1095ms, Memory: 32MB, Status: Time Limit Exceed - Codetree

--- 251028/최솟값 3개/top-3-smallest-number.py
+++ 251028/최솟값 3개/top-3-smallest-number.py
@@ -9,7 +9,7 @@ for i in range(n):
         print(-1)
     else:
         ans = 1
-        arr_copy = arr.copy()
+        arr_copy = arr[:i + 1]
         for _ in range(3):
             ans *= heapq.heappop(arr_copy)
         print(ans)

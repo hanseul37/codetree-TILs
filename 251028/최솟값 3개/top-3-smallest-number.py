5e3e062bde90532ca1f788683f3926ac2a42d55a
hanseul37 <hanseul37@daum.net>
2025-10-28 15:48:59 +0900

    Title: 최솟값 3개, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251028/최솟값 3개/top-3-smallest-number.py
+++ 251028/최솟값 3개/top-3-smallest-number.py
@@ -3,19 +3,15 @@ import heapq
 n = int(input())
 arr = list(map(int, input().split()))
 pq = []
-first, second = -1, -1
 for i in range(n):
+    heapq.heappush(pq, arr[i])
     if i <= 1:
         print(-1)
-        if i == 1:
-            first = min(arr[0], arr[1])
-            second = max(arr[0], arr[1])
     else:
-        if second > arr[i]:
-            heapq.heappush(pq, second)
-            second = arr[i]
-        else:
-            heapq.heappush(pq, arr[i])
-        print(first * second * pq[0])
+        ans = 1
+        arr_copy = arr.copy()
+        for _ in range(3):
+            ans *= heapq.heappop(arr_copy)
+        print(ans)
 
 

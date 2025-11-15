1ca313885d880d18e05ad8c36c796cc3c4ece448
hanseul37 <hanseul37@daum.net>
2025-11-16 00:06:29 +0900

    Title: 강강수월래, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251116/강강수월래/circle-dance.py
+++ 251116/강강수월래/circle-dance.py
@@ -6,96 +6,80 @@ class Node:
 
 n, m, q = map(int, input().split())
 
-circle = []          # 각 원의 dict
-circle_id = {}       # 학생 → 원 인덱스
-circle_min = []      # 각 원의 최소 학생 번호
+circle_head = {}  # 원의 대표 노드
+node_map = {}     # 학생 -> 노드
 
 for i in range(m):
     vals = list(map(int, input().split()))
     length, points = vals[0], vals[1:]
-    nodes = {st: Node(st) for st in points}
-
-    for j in range(length):
-        nodes[points[j]].next = nodes[points[(j+1)%length]]
-        nodes[points[j]].prev = nodes[points[(j-1)%length]]
-
-    circle.append(nodes)
-    circle_min.append(min(points))
+    prev_node = None
+    first_node = None
     for st in points:
-        circle_id[st] = i
+        node = Node(st)
+        node_map[st] = node
+        if prev_node:
+            prev_node.next = node
+            node.prev = prev_node
+        else:
+            first_node = node
+        prev_node = node
+    # 원형 연결
+    prev_node.next = first_node
+    first_node.prev = prev_node
+    # 대표 노드 기록
+    for st in points:
+        circle_head[st] = first_node
 
 for _ in range(q):
     op = list(map(int, input().split()))
-
     if op[0] == 1:  # merge a -> b
         a, b = op[1], op[2]
-        ca, cb = circle_id[a], circle_id[b]
-        if ca == cb:
+        head_a = circle_head[a]
+        head_b = circle_head[b]
+        if head_a == head_b:
             continue
-
-        node_a = circle[ca][a]
-        node_b = circle[cb][b]
-        a_next = node_a.next
-        b_prev = node_b.prev
-
         # 연결
+        a_next = node_map[a].next
+        b_prev = node_map[b].prev
         a_next.prev = b_prev
         b_prev.next = a_next
-        node_a.next = node_b
-        node_b.prev = node_a
+        node_map[a].next = node_map[b]
+        node_map[b].prev = node_map[a]
 
-        # 학생 ID만 갱신
-        for st in circle[cb]:
-            circle_id[st] = ca
-            circle[ca][st] = circle[cb][st]
-
-        circle[cb] = {}
-        circle_min[ca] = min(circle_min[ca], circle_min[cb])
+        # circle_head 갱신
+        cur = node_map[b]
+        while True:
+            circle_head[cur.data] = head_a
+            cur = cur.next
+            if cur == node_map[b]:
+                break
 
     elif op[0] == 2:  # split a <-> b
         a, b = op[1], op[2]
-        ci = circle_id[a]
-        if circle_id[b] != ci:
+        if circle_head[a] != circle_head[b]:
             continue
+        a_prev = node_map[a].prev
+        b_prev = node_map[b].prev
+        a_prev.next = node_map[b]
+        b_prev.next = node_map[a]
+        node_map[a].prev = b_prev
+        node_map[b].prev = a_prev
 
-        node_a = circle[ci][a]
-        node_b = circle[ci][b]
-        a_prev = node_a.prev
-        b_prev = node_b.prev
-
-        # 연결
-        a_prev.next = node_b
-        b_prev.next = node_a
-        node_a.prev = b_prev
-        node_b.prev = a_prev
-
-        # 새로운 원 생성
-        new_circle = {}
-        cur = node_b
-        new_min = cur.data
+        # 새 원 대표 노드 갱신
+        cur = node_map[b]
         while True:
-            new_circle[cur.data] = cur
-            circle_id[cur.data] = len(circle)
-            new_min = min(new_min, cur.data)
+            circle_head[cur.data] = node_map[b]
             cur = cur.next
-            if cur == node_b:
+            if cur == node_map[b]:
                 break
-        circle.append(new_circle)
-        circle_min.append(new_min)
-
-        # 기존 원에서 삭제
-        for st in new_circle:
-            del circle[ci][st]
-        circle_min[ci] = min(circle[ci].keys()) if circle[ci] else 10**9
 
     elif op[0] == 3:  # print
         a = op[1]
-        ci = circle_id[a]
-        start = circle_min[ci]
-        cur = circle[ci][start]
+        head = circle_head[a]
+        cur = head
         while True:
             print(cur.data, end=' ')
             cur = cur.prev
-            if cur.data == start:
+            if cur == head:
                 break
         print()

f4380dad972b9ce5ad099a433c86c62d1657b204
hanseul37 <hanseul37@daum.net>
2025-11-15 23:59:05 +0900

    Title: 강강수월래, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251115/강강수월래/circle-dance.py
+++ 251115/강강수월래/circle-dance.py
@@ -2,10 +2,12 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.prev = None
-        self.next = None 
+        self.next = None
 
 n, m, q = map(int, input().split())
 circle, circle_id = [], {}
+circle_mins = [] 
+
 for i in range(m):
     circle_len, *points = map(int, input().split())
     students = {st:Node(st) for st in points}
@@ -13,6 +15,7 @@ for i in range(m):
         students[points[j]].prev = students[points[j - 1]]
         students[points[j - 1]].next = students[points[j]]
     circle.append(students)
+    circle_mins.append(min(points))
     for st in points:
         circle_id[st] = i
 
@@ -29,6 +32,9 @@ for _ in range(q):
         if len(circle[circle_a_idx]) < len(circle[circle_b_idx]):
             a, b = b, a
             circle_a_idx, circle_b_idx = circle_b_idx, circle_a_idx
+        
+        old_min_a = circle_mins[circle_a_idx]
+        old_min_b = circle_mins[circle_b_idx]
 
         a_next = circle[circle_a_idx][a].next
         b_prev = circle[circle_b_idx][b].prev
@@ -44,12 +50,18 @@ for _ in range(q):
         
         circle[circle_a_idx].update(circle[circle_b_idx])
         circle[circle_b_idx] = {}
-    
+        
+        circle_mins[circle_a_idx] = min(old_min_a, old_min_b)
+        circle_mins[circle_b_idx] = None
+
     elif op[0] == 2:
         a, b = op[1], op[2]
-        if circle_id[a] != circle_id[b]:
+        if a not in circle_id or b not in circle_id or circle_id[a] != circle_id[b]:
             continue
         circle_idx = circle_id[a]
+        
+        old_min = circle_mins[circle_idx]
+
         a_prev, b_prev = circle[circle_idx][a].prev, circle[circle_idx][b].prev
         a_prev.next = circle[circle_idx][b]
         b_prev.next = circle[circle_idx][a]
@@ -64,19 +76,34 @@ for _ in range(q):
             cur = cur.next
             if cur == circle[circle_idx][b]:
                 break
-        circle.append(new_circle)
-
+        
         for student in new_circle:
             del circle[circle_idx][student]
-    
+        
+        circle.append(new_circle)
+        
+        circle_mins.append(min(new_circle.keys()))
+        if old_min in new_circle:
+            if circle[circle_idx]:
+                circle_mins[circle_idx] = min(circle[circle_idx].keys())
+            else:
+                circle_mins[circle_idx] = None
+
     elif op[0] == 3:
         a = op[1]
+        if a not in circle_id:
+            continue
         circle_idx = circle_id[a]
-        min_student = min(circle[circle_idx].keys())
+        if not circle[circle_idx]:
+            continue
+
+        min_student = circle_mins[circle_idx]
         cur = circle[circle_idx][min_student]
+        
+        start_node = cur
         while True:
             print(cur.data, end=' ')
             cur = cur.prev
-            if cur == circle[circle_idx][min_student]:
+            if cur == start_node:
                 break
-        
+        print()

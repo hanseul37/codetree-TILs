de31e5f934317fd260b57584630324fcbe343d02
hanseul37 <hanseul37@daum.net>
2025-12-01 16:59:04 +0900

    Title: 겹치지 않는 선분, Time: 218ms, Memory: 30MB, Status: Passed - Codetree

--- 251201/겹치지 않는 선분/line-segments-that-do-not-overlap.py
+++ 251201/겹치지 않는 선분/line-segments-that-do-not-overlap.py
@@ -6,7 +6,7 @@ arr.sort()
 left[0], right[-1] = arr[0][1], arr[-1][1]
 for i in range(1, n):
     left[i] = max(left[i - 1], arr[i][1])
-    right[n - 1 - i] = min(right[n - i], arr[i][1])
+    right[n - 1 - i] = min(right[n - i], arr[n - i - 1][1])
 
 cnt = 0
 for i in range(n):

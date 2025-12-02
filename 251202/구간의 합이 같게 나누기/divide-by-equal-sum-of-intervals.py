39d1e8d451e1485c45e8d32a65d32f9e438e15ba
hanseul37 <hanseul37@daum.net>
2025-12-02 14:57:16 +0900

    Title: 구간의 합이 같게 나누기, Time: 83ms, Memory: 28MB, Status: Wrong Answer - Codetree

--- 251202/구간의 합이 같게 나누기/divide-by-equal-sum-of-intervals.py
+++ 251202/구간의 합이 같게 나누기/divide-by-equal-sum-of-intervals.py
@@ -2,26 +2,27 @@ from math import comb
 
 n = int(input())
 arr = list(map(int, input().split()))
-
-if sum(arr) == 0:
-    print(comb(n - 1, 3))
-    exit()
-
 prefix, left, right = [0] * n, [0] * n, [0] * n
 prefix[0] = arr[0]
 for i in range(1, n):
     prefix[i] = prefix[i - 1] + arr[i]
 
+total = prefix[-1]
+t1, t2, t3 = total // 4, total // 4 * 2, total // 4 * 3
+if total == 0:
+    print(comb(n - 1, 3))
+    exit()
+
 l_cnt, r_cnt = 0, 0
 for i in range(n):
-    if prefix[i] == sum(arr) // 4:
+    if prefix[i] == t1:
         l_cnt += 1
-    if prefix[n - 1 - i] == sum(arr) // 4 * 3:
+    if prefix[n - 1 - i] == t3:
         r_cnt += 1
     left[i], right[i] = l_cnt, r_cnt
 
 cnt = 0
 for i in range(n):
-    if prefix[i] == sum(arr) // 2:
+    if prefix[i] == t2:
         cnt += left[i] * right[i]
 print(cnt)

2ad7eea9d5f2dbd059ab66318121293332ef2d2f
hanseul37 <hanseul37@daum.net>
2025-12-03 15:51:26 +0900

    Title: 점 개수 세기, Time: 421ms, Memory: 27MB, Status: Passed - Codetree

--- 251203/점 개수 세기/count-number-of-points.py
+++ 251203/점 개수 세기/count-number-of-points.py
@@ -2,6 +2,7 @@ from bisect import bisect_left, bisect_right
 
 n, q = map(int, input().split())
 points = list(map(int, input().split()))
+points.sort()
 
 for _ in range(q):
     a, b = map(int, input().split())

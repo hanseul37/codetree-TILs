6699e313763366b418de8ce130697deecd939dcd
hanseul37 <hanseul37@daum.net>
2025-12-10 23:29:52 +0900

    Title: 컴퓨터 이용시간, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251210/컴퓨터 이용시간/computer-hours.py
+++ 251210/컴퓨터 이용시간/computer-hours.py
@@ -7,20 +7,23 @@ for i in range(n):
 
 arr.sort()
 
-computer = [0]*n
-ans = [0]*n
+computer = [0] * n
+ans = [0] * n
 next_free = 0
 
 for time, person, flag in arr:
-    if flag == 1:  # 입장
-        # next_free부터 가능한 곳을 찾기
-        while computer[next_free] != 0:
+    if flag == 1:  # 들어오는 순간
+        # next_free가 범위를 넘지 않도록 보장
+        while next_free < n and computer[next_free] != 0:
             next_free += 1
+        # 반드시 빈 자리가 있어야 함
         computer[next_free] = person
         ans[person] = next_free + 1
-    else:  # 퇴장
+
+    else:  # 나가는 순간
         idx = ans[person] - 1
         computer[idx] = 0
+        # next_free를 최소 위치로 되돌림
         if idx < next_free:
             next_free = idx
 

5859a2969d366575679128954a13dd77c2919637
hanseul37 <hanseul37@daum.net>
2025-12-10 23:42:51 +0900

    Title: 컴퓨터 이용시간, Time: 662ms, Memory: 40MB, Status: Passed - Codetree

--- 251210/컴퓨터 이용시간/computer-hours.py
+++ 251210/컴퓨터 이용시간/computer-hours.py
@@ -1,30 +1,25 @@
+import heapq
+
 n = int(input())
 arr = []
 for i in range(n):
-    s, e = map(int, input().split())
-    arr.append((s, i, 1))
-    arr.append((e, i, -1))
-
+    start, end = map(int, input().split())
+    arr.append([start, i + 1, 1])
+    arr.append([end, i + 1, -1])
 arr.sort()
 
-computer = [0] * n
+free_computer= list(range(n))
+heapq.heapify(free_computer)
+
 ans = [0] * n
-next_free = 0
-
+computer = {}
 for time, person, flag in arr:
-    if flag == 1:  # 들어오는 순간
-        # next_free가 범위를 넘지 않도록 보장
-        while next_free < n and computer[next_free] != 0:
-            next_free += 1
-        # 반드시 빈 자리가 있어야 함
-        computer[next_free] = person
-        ans[person] = next_free + 1
-
-    else:  # 나가는 순간
-        idx = ans[person] - 1
-        computer[idx] = 0
-        # next_free를 최소 위치로 되돌림
-        if idx < next_free:
-            next_free = idx
+    if flag == 1:
+        idx = heapq.heappop(free_computer)
+        ans[person - 1] = idx + 1
+        computer[person] = idx
+    else:
+        idx = computer.pop(person)
+        heapq.heappush(free_computer, idx)
 
 print(*ans)

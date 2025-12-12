0f0a7e08aae87da05acc111ea2a32a31c7534bd4
hanseul37 <hanseul37@daum.net>
2025-12-12 17:44:46 +0900

    Title: 선분 바라보기 2, Time: 85ms, Memory: 19MB, Status: Wrong Answer - Codetree

--- 251212/선분 바라보기 2/looking-at-the-line-segment-2.py
+++ 251212/선분 바라보기 2/looking-at-the-line-segment-2.py
@@ -4,20 +4,30 @@ n = int(input())
 arr = []
 for i in range(n):
     y, x1, x2 = map(int, input().split())
-    arr.append([x1, y, i, 1])
-    arr.append([x2, y, i, -1])
-arr.sort()
+    arr.append([x1, y, i, 1])   # start
+    arr.append([x2, y, i, -1])  # end
+
+# 정렬: x 증가, 같은 x라면 start(v=1) 먼저 처리하도록 (-v)
+arr.sort(key=lambda e: (e[0], -e[3]))
 
 cnt = 0
 color = SortedList()
+
 for x, y, idx, v in arr:
+    # before 상태 저장
+    before = color[0] if color else None
+
     if v == 1:
         color.add((y, idx))
-        if color[0][1] == idx:
-            cnt += 1
     else:
-        if len(color) > 1 and color[0][1] == idx:
-            cnt += 1
+        # 제거 (항상 존재한다고 가정)
         color.remove((y, idx))
-print(cnt)
 
+    # after 상태 저장
+    after = color[0] if color else None
+
+    # 전면이 바뀌었으면 +1
+    if before != after:
+        cnt += 1
+
+print(cnt)

1045a29556dab5c8fff1a5a4e9de48c899000197
hanseul37 <hanseul37@daum.net>
2025-12-13 18:48:04 +0900

    Title: 폭탄 해체 작업, Time: 110ms, Memory: 26MB, Status: Passed - Codetree

--- 251213/폭탄 해체 작업/the-bomb-dismantling.py
+++ 251213/폭탄 해체 작업/the-bomb-dismantling.py
@@ -1,10 +1,11 @@
+import heapq
+
 n = int(input())
 bombs = [list(map(int, input().split())) for _ in range(n)]
-bombs.sort(key=lambda x:(x[1], -x[0]))
-time, total = 0, 0
+bombs.sort(key=lambda x:x[1])
+arr = []
 for point, limit in bombs:
-    if time < limit:
-        total += point
-        time += 1
-print(total)
-
+    heapq.heappush(arr, point)
+    if len(arr) > limit:
+        heapq.heappop(arr)
+print(sum(arr))

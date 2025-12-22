2a7bab6592722a64566944583b25198e13e4cf65
hanseul37 <hanseul37@daum.net>
2025-12-22 11:52:57 +0900

    Title: 이상한 폭탄, Time: 129ms, Memory: 25MB, Status: Passed - Codetree

--- 251222/이상한 폭탄/strange-bomb.py
+++ 251222/이상한 폭탄/strange-bomb.py
@@ -1,13 +1,14 @@
-from collections import deque
-
 n, k = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
-window = deque()
+window = dict()
 max_num = -1
 for i in range(n):
     if i > k:
-        window.popleft()
+        window[arr[i - k - 1]] -= 1
     if arr[i] in window:
+        window[arr[i]] += 1
+    else:
+        window[arr[i]] = 1
+    if window[arr[i]] > 1:
         max_num = max(arr[i], max_num)
-    window.append(arr[i])
 print(max_num)

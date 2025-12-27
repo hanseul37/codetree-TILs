9f2bcd2253677936f88785402234c4da825de45a
hanseul37 <hanseul37@daum.net>
2025-12-27 10:38:02 +0900

    Title: 색칠된 칸으로만 이동 2, Time: 1095ms, Memory: 25MB, Status: Time Limit Exceed - Codetree

---
+++ 251227/색칠된 칸으로만 이동 2/move-to-the-colored-space-only-2.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+m, n = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(m)]
+colored = [list(map(int, input().split())) for _ in range(m)]
+colored_cnt, color = 0, []
+for i in range(m):
+    for j in range(n):
+        if colored[i][j] == 1:
+            colored_cnt += 1
+            color.append([i, j])
+
+def check(d):
+    for y, x in color:
+        q = deque([[y, x]])
+        dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+        visited = [[False] * n for _ in range(m)]
+        visited[y][x] = True
+        cnt = 1
+        while q:
+            r, c = q.popleft()
+            for dx, dy in zip(dxs, dys):
+                nx, ny = c + dx, r + dy
+                if 0 <= ny < m and 0 <= nx < n and not visited[ny][nx] and abs(board[ny][nx] - board[r][c]) <= d:
+                    q.append([ny, nx])
+                    visited[ny][nx] = True
+                    if colored[ny][nx] == 1:
+                        cnt += 1
+        if cnt < colored_cnt:
+            return False
+    else:
+        return True
+
+left, right = 0, 10 ** 9
+while left <= right:
+    mid = (left + right) // 2
+    if check(mid):
+        right = mid - 1
+    else:
+        left = mid + 1
+print(left)
+    

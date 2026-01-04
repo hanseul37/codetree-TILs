02b70cc8083c589056993ab3bd40d79632896fc6
hanseul37 <hanseul37@daum.net>
2026-01-04 20:26:13 +0900

    Title: 최소가 되는 좌표 범위의 차, Time: 487ms, Memory: 24MB, Status: Passed - Codetree

--- 260104/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.py
+++ 260104/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.py
@@ -2,16 +2,22 @@ n, d = map(int, input().split())
 points = [list(map(int, input().split())) for _ in range(n)]
 points.sort()
 left, min_range = 0, 1000001
-for right in range(1, n):
-    for left in range(right - 1, -1, -1):
-        if points[right][0] - points[left][0] >= min_range:
-            break   
-        min_y, max_y = points[right][1], points[right][1]
-        for i in range(left, right):
-            min_y = min(points[i][1], min_y)
-            max_y = max(points[i][1], max_y)
-        if max_y - min_y >= d:
-            min_range = points[right][0] - points[left][0]
+min_q, max_q = [], []
+for right in range(n):
+    while min_q and min_q[-1] > points[right][1]:
+        min_q.pop()
+    min_q.append(points[right][1])
+    while max_q and max_q[-1] < points[right][1]:
+        max_q.pop()
+    max_q.append(points[right][1])
+    
+    while max_q[0] - min_q[0] >= d:
+        min_range = min(points[right][0] - points[left][0], min_range)
+        if min_q[0] == points[left][1]:
+            min_q.pop(0)
+        if max_q[0] == points[left][1]:
+            max_q.pop(0)
+        left += 1
 if min_range == 1000001:
     print(-1)
 else:

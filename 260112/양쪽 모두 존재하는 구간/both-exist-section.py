79aa2e1f5d64f0a8bbdcb12e536031860084fc48
hanseul37 <hanseul37@daum.net>
2026-01-12 15:01:50 +0900

    Title: 양쪽 모두 존재하는 구간, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260112/양쪽 모두 존재하는 구간/both-exist-section.py
+++ 260112/양쪽 모두 존재하는 구간/both-exist-section.py
@@ -1,6 +1,6 @@
 n, m = map(int, input().split())
 arr = [0] + list(map(int, input().split()))
-cnt, right, min_part = [0] * (m + 1), 1, n
+cnt, right, min_part = [0] * (m + 1), 1, n + 1
 for left in range(1, n + 1):
     cnt[arr[left - 1]] -= 1
     while right <= n and 0 in cnt:
@@ -8,4 +8,7 @@ for left in range(1, n + 1):
         right += 1
     if 0 not in cnt:
         min_part = min(right - left, min_part)
-print(min_part)
+if min_part == n + 1:
+    print(-1)
+else:
+    print(min_part)

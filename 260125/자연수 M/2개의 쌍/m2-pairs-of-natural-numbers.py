82ba33ec0009c9a420579e73849f7116715be110
hanseul37 <hanseul37@daum.net>
2026-01-25 20:20:14 +0900

    Title: 자연수 M/2개의 쌍, Time: 578ms, Memory: 30MB, Status: Passed - Codetree

--- 260125/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
+++ 260125/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -2,10 +2,29 @@ n = int(input())
 arr = []
 for _ in range(n):
     x, y = map(int, input().split())
-    for _ in range(x):
-        arr.append(y)
+    arr.append([y, x])
 arr.sort()
-c = 10 ** 9 * 2
-for i in range(len(arr) // 2):
-    c = min(arr[i] + arr[len(arr) - 1 - i], c)
+l, r, c = 0, n - 1, 0
+while l <= r:
+    if l == r:
+        c = max(c, arr[l][0] * 2)
+        break
+
+    k = min(arr[l][1], arr[r][1])
+    if k == 0:
+        if arr[l][1] == 0:
+            l += 1
+        if arr[r][1] == 0:
+            r -= 1
+        continue
+
+    c = max(c, arr[l][0] + arr[r][0])
+    arr[l][1] -= k
+    arr[r][1] -= k
+
+    if arr[l][1] == 0:
+        l += 1
+    if arr[r][1] == 0:
+        r -= 1
+
 print(c)

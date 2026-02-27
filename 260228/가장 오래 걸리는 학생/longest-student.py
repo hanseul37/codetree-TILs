f6a1ca847d0eb870bd1949564ad90383e58f0404
hanseul37 <hanseul37@daum.net>
2026-02-28 00:23:12 +0900

    Title: 가장 오래 걸리는 학생, Time: 880ms, Memory: 48MB, Status: Passed - Codetree

--- 260228/가장 오래 걸리는 학생/longest-student.py
+++ 260228/가장 오래 걸리는 학생/longest-student.py
@@ -1,37 +1,23 @@
 import heapq
-import sys
-input = sys.stdin.readline
 
-N, M = map(int, input().split())
-graph = [[] for _ in range(N)]
+n, m = map(int, input().split())
+graph = [[] for _ in range(n)]
+for _ in range(m):
+    v1, v2, weight = map(int, input().split())
+    graph[v1 - 1].append([v2 - 1, weight])
+    graph[v2 - 1].append([v1 - 1, weight])
 
-for _ in range(M):
-    i, j, d = map(int, input().split())
-    i -= 1
-    j -= 1
-    graph[i].append((j, d))
-    graph[j].append((i, d))  # 무방향
+distance = [float('inf')] * n
+distance[n - 1] = 0
+q = [[0, n - 1]]
+while q:
+    dist, node = heapq.heappop(q)
+    if dist > distance[node]:
+        continue
+    for next_node, weight in graph[node]:
+        cost = dist + weight
+        if cost < distance[next_node]:
+            distance[next_node] = cost
+            heapq.heappush(q, [cost, next_node])
 
-def dijkstra(start):
-    dist = [float('inf')] * N
-    dist[start] = 0
-    pq = [(0, start)]
-
-    while pq:
-        cur_dist, node = heapq.heappop(pq)
-        if cur_dist > dist[node]:
-            continue
-
-        for next_node, weight in graph[node]:
-            new_dist = cur_dist + weight
-            if new_dist < dist[next_node]:
-                dist[next_node] = new_dist
-                heapq.heappush(pq, (new_dist, next_node))
-
-    return dist
-
-# 학교는 N번 → 인덱스 N-1
-distances = dijkstra(N - 1)
-
-# 1번 ~ N-1 중 최댓값
-print(max(distances[:-1]))
+print(max(distance))

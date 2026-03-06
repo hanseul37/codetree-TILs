9a95326eb4f26c17e94bb7467e3543e282147ab2
hanseul37 <hanseul37@daum.net>
2026-03-06 18:36:23 +0900

    Title: 도전! 369게임, Time: 1085ms, Memory: 24MB, Status: Time Limit Exceed - Codetree

--- 260306/도전! 369게임/369-game-challenge.py
+++ 260306/도전! 369게임/369-game-challenge.py
@@ -1,21 +1,19 @@
 n = input()
+dp = [[0, 0] for _ in range(3)]
+dp[0][0] = 1
+for c in n:
+    new_dp = [[0, 0] for _ in range(3)]
+    for i in range(3):
+        for j in range(2):
+            if not dp[i][j]:
+                continue
+            for k in range(10):
+                if k in [3, 6, 9]:
+                    continue
+                if j == 0 and k > int(c):
+                    continue
+                new_dp[(i + k) % 3][j or k < int(c)] += dp[i][j]
+    dp = new_dp
+print((int(n) - sum(dp[1]) - sum(dp[2])) % (10**9 + 7))
 
-def dfs(pos, mod, tight):
-    if pos == len(n):
-        if mod != 0:
-            return 1
-        else:
-            return 0
-    if tight:
-        limit = int(n[pos])
-    else:
-        limit = 9
-    res = 0
-    for i in range(limit + 1):
-        if i in [3, 6, 9]:
-            continue
-        new_tight = tight and i == limit
-        res += dfs(pos + 1, (mod + i) % 3, new_tight)
-    return res
-
-print(int(n) - dfs(0, 0, True))
+                

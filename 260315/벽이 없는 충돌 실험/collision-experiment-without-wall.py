001e3b66553ca6678164c78d4a0010ee2fe471ff
hanseul37 <hanseul37@daum.net>
2026-03-15 18:23:30 +0900

    Title: 벽이 없는 충돌 실험, Time: 4091ms, Memory: 24MB, Status: Time Limit Exceed - Codetree

--- 260315/벽이 없는 충돌 실험/collision-experiment-without-wall.py
+++ 260315/벽이 없는 충돌 실험/collision-experiment-without-wall.py
@@ -1,52 +1,50 @@
-t = int(input())
+import sys
 
+# 입력 속도 최적화
+input = sys.stdin.readline
+
+t = int(input())
 for _ in range(t):
     n = int(input())
-
-    x, y, w, d = [], [], [], []
-    alive = [True] * n
-
+    beads = [] 
+    
     for i in range(n):
         xi, yi, wi, di = input().split()
-        x.append((int(xi) + 1000) * 2)
-        y.append((-int(yi) + 1000) * 2)
-        w.append(int(wi))
-        d.append(di)
+        x = (int(xi) + 1000) * 2
+        # 🚨 여기에 빠졌던 마이너스(-) 부호를 복구했습니다!
+        y = (-int(yi) + 1000) * 2 
+        w = int(wi)
+        beads.append([x, y, w, di, i]) 
 
     last_collision = -1
-
-    for time in range(1, 4001):
-
+    
+    for cnt in range(1, 4001):
+        if len(beads) <= 1:
+            break
+            
         pos = {}
-
-        for i in range(n):
-            if not alive[i]:
-                continue
-
-            if d[i] == 'U':
-                y[i] -= 1
-            elif d[i] == 'D':
-                y[i] += 1
-            elif d[i] == 'L':
-                x[i] -= 1
+        is_collided = False
+        
+        for b in beads:
+            if b[3] == 'U': b[1] -= 1
+            elif b[3] == 'D': b[1] += 1
+            elif b[3] == 'R': b[0] += 1
+            elif b[3] == 'L': b[0] -= 1
+            
+            key = (b[0], b[1])
+            
+            if key not in pos:
+                pos[key] = b
             else:
-                x[i] += 1
-
-            key = (x[i], y[i])
-            pos.setdefault(key, []).append(i)
-
-        collision = False
-
-        for arr in pos.values():
-            if len(arr) > 1:
-                collision = True
-                arr.sort(key=lambda i: (w[i], i), reverse=True)
-                winner = arr[0]
-
-                for i in arr[1:]:
-                    alive[i] = False
-
-        if collision:
-            last_collision = time
+                is_collided = True
+                existing = pos[key]
+                # 무게가 크거나, 무게가 같을 때 고유 번호가 큰 구슬이 생존
+                if b[2] > existing[2] or (b[2] == existing[2] and b[4] > existing[4]):
+                    pos[key] = b
+                    
+        if is_collided:
+            last_collision = cnt
+            
+        beads = list(pos.values())
 
     print(last_collision)

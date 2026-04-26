bfa19021193f36bf926d9e015b8b1f5d9c5c00da
hanseul37 <hanseul37@daum.net>
2026-04-26 23:54:33 +0900

    Title: 인접한 정점, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260426/인접한 정점/adjacent-node.py
@@ -0,0 +1,21 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+n = int(input())
+values = list(map(int, input().split()))
+tree, dp = [[] for _ in range(n)], [[0, 0] for _ in range(n)]
+for _ in range(n - 1):
+    v1, v2 = map(int, input().split())
+    tree[v1 - 1].append(v2 - 1)
+    tree[v2 - 1].append(v1 - 1)
+
+def dfs(node, parent):
+    dp[node][1] = values[node]
+    for next_node in tree[node]:
+        if next_node != parent:
+            dfs(next_node, node)
+            dp[node][0] += max(dp[next_node][0], dp[next_node][1])
+            dp[node][1] += dp[next_node][0]
+
+dfs(0, -1)
+print(max(dp[0][0], dp[0][1]))

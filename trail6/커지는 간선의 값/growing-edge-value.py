7023a2b2b2881e5830caa558a422d51edba570b8
hanseul37 <hanseul37@daum.net>
2026-07-02 23:45:09 +0900

    [Passed] 커지는 간선의 값 (798ms, 45MB)

---
+++ trail6/커지는 간선의 값/growing-edge-value.py
@@ -0,0 +1,26 @@
+n, m, k = map(int, input().split())
+edges, arr = [], [i for i in range(n)]
+
+for _ in range(m):
+    v1, v2, weight = map(int, input().split())
+    edges.append([v1 - 1, v2 - 1, weight])
+edges.sort(key=lambda x:x[2])
+
+def find(x):
+    if arr[x] == x:
+        return x
+    arr[x] = find(arr[x])
+    return arr[x]
+
+def union(x, y):
+    arr[find(x)] = find(y)
+
+cnt, edge_cnt = 0, 0
+for v1, v2, weight in edges:
+    if find(v1) != find(v2):
+        union(v1, v2)
+        cnt += weight + edge_cnt * k
+        edge_cnt += 1
+    if edge_cnt == n - 1:
+        break
+print(cnt)

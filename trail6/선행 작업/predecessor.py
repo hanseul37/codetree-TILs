23f3095678d70ab2f39fc7241fd717efe2698e0a
hanseul37 <hanseul37@daum.net>
2026-07-28 00:08:33 +0900

    [Passed] 선행 작업 (116ms, 24MB)

--- trail6/선행 작업/predecessor.py
+++ trail6/선행 작업/predecessor.py
@@ -1,26 +1,25 @@
 from collections import deque
 
 n = int(input())
-graph, in_degree, arr, result = [[] for _ in range(n)], [0] * n, [], [0] * n 
+graph, in_degree, arr, dp = [[] for _ in range(n)], [0] * n, [], [0] * n 
 for i in range(n):
     cost, _, *works = map(int, input().split())
-    for work in works:
-        graph[work - 1].append(i) 
+    for w in works:
+        graph[w - 1].append(i) 
         in_degree[i] += 1
     arr.append(cost)
-    result[i] = cost
+    dp[i] = cost
 
 q = deque() 
 for i in range(n):
     if in_degree[i] == 0:
         q.append(i)
 
-cnt = 0
 while q:
     node = q.popleft()
     for next_node in graph[node]:
+        dp[next_node] = max(dp[next_node], dp[node] + arr[next_node])
         in_degree[next_node] -= 1
-        result[next_node] = max(result[next_node], result[node] + arr[next_node])
         if in_degree[next_node] == 0:
             q.append(next_node)
-print(max(result))
+print(max(dp))
